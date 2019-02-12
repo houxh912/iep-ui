a7f2b9da1fe4325708a99c94b426a03653cb4f42
杜照鸿 <12345678@qq.com>
2019-02-12 17:10:03 +0800

    gds假数据修改

--- server/router.js
+++ server/router.js
@@ -1,5 +1,6 @@
 const Router = require('koa-router')
 const codeController = require('./controller/code')
+const authController = require('./controller/auth')
 const adminController = require('./controller/admin')
 const router = new Router()
 /**
@@ -11,6 +12,8 @@ router.get('/', (ctx) => {
   ctx.body = 'Hello World'
 })
 router.get('/code', codeController.getCode)
+router.post('/auth/oauth/token', authController.getOauthToken)
 router.get('/admin/user/info', adminController.getUserInfo)
+router.get('/admin/menu', adminController.getMenu)
 
 exports = module.exports = router

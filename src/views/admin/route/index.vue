34dedab3d30966314c1c2e41224a2ab30a7109b4
张超 <zhangchao@126.com>
2019-02-23 15:37:09 +0800

    feat(路由添加功能):

--- src/views/admin/route/index.vue
+++ src/views/admin/route/index.vue
@@ -5,19 +5,54 @@
       </el-alert>
       <vue-json-editor v-model="json" :show-btns="false"></vue-json-editor>
       <div align="center">
-        <el-button @click="edit()">更新</el-button>
+        <el-button @click="openAddDialog()">添加路由</el-button>
+        <el-button @click="edit()">更新路由</el-button>
       </div>
     </basic-container>
+    <el-dialog title="新增路由" :visible.sync="dialogFormVisible">
+      <el-form class="router-form" :model="form" label-width="100px">
+        <el-form-item label="路由名称">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="路由英文">
+          <el-input v-model="form.english" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="路由值">
+          <el-input v-model="form.path" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="add()">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 <script>
 import vueJsonEditor from 'vue-json-editor'
 import { fetchList, putObj } from '@/api/admin/route'
-
+const tempPath = (form) => `
+{
+  "routeId": "${form.english}",
+  "routeName": "${form.name}",
+  "predicates": "[{"args": {"_genkey_0": "${form.path}"}, "name": "Path"}]",
+  "filters": "[]",
+  "uri": "lb://${form.english}",
+  "order": 0,
+  "createTime": "2019-02-23 10:40:11",
+  "updateTime": null,
+  "delFlag": "0"
+},`
 export default {
   data () {
     return {
       json: null,
+      dialogFormVisible: false,
+      form: {
+        name: 'gds-codegen',
+        english: 'gds-codegen',
+        path: '/gen/**',
+      },
     }
   },
 
@@ -48,6 +83,22 @@ export default {
         this.json = result
       })
     },
+    add () {
+      console.log(this.json, JSON.parse(tempPath(this.form)))
+      const json = [...this.json, JSON.parse(tempPath(this.form))]
+      putObj(json).then(() => {
+        this.$notify({
+          title: '成功',
+          message: '更新成功',
+          type: 'success',
+          duration: 2000,
+        })
+      })
+      this.dialogFormVisible = false
+    },
+    openAddDialog () {
+      this.dialogFormVisible = true
+    },
 
     edit () {
       putObj(this.json).then(() => {
@@ -62,3 +113,8 @@ export default {
   },
 }
 </script>
+<style lang="scss" scoped>
+.router-form {
+  padding: 0 40px;
+}
+</style>

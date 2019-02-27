015f00dc12d13d44bc6f147335570b6fae591840
张超 <zhangchao@126.com>
2019-02-27 15:49:24 +0800

    ADD 人力页面模板

--- src/views/hrms/OrganizationalStructure/JobTitleSystem/index.vue
+++ src/views/hrms/OrganizationalStructure/JobTitleSystem/index.vue
@@ -1,3 +1,37 @@
 <template>
-  <div>demo</div>
+  <div>
+    <basic-container>
+      <page-header title="职务体系" :replaceText="replaceText" :data="[20]"></page-header>
+      <iep-tabs v-model="activeTab" :tab-list="tabList">
+        <template v-if="activeTab ==='JobSystem'" v-slot:JobSystem>
+          <job-system v-loading="activeTab !=='JobSystem'"></job-system>
+        </template>
+        <template v-if="activeTab ==='TitleSystem'" v-slot:TitleSystem>
+          <title-system v-loading="activeTab !=='TitleSystem'"></title-system>
+        </template>
+      </iep-tabs>
+    </basic-container>
+  </div>
 </template>
+<script>
+import PageHeader from '@/components/Page/Header'
+import IepTabs from '@/components/IepCommon/Tabs'
+import JobSystem from './JobSystem/'
+import TitleSystem from './TitleSystem/'
+export default {
+  components: { PageHeader, IepTabs, JobSystem, TitleSystem },
+  data () {
+    return {
+      replaceText: (data) => `（共有${data[0]}个岗位`,
+      tabList: [{
+        label: '职务体系',
+        value: 'JobSystem',
+      }, {
+        label: '职称体系',
+        value: 'TitleSystem',
+      }],
+      activeTab: 'JobSystem',
+    }
+  },
+}
+</script>
--- src/views/hrms/OrganizationalStructure/PostLibrary/index.vue
+++ src/views/hrms/OrganizationalStructure/PostLibrary/index.vue
@@ -1,3 +1,37 @@
 <template>
-  <div>demo</div>
+  <div>
+    <basic-container>
+      <page-header title="岗位库" :replaceText="replaceText" :data="[20]"></page-header>
+      <iep-tabs v-model="activeTab" :tab-list="tabList">
+        <template v-if="activeTab ==='PostLibrary'" v-slot:PostLibrary>
+          <post-library v-loading="activeTab !=='PostLibrary'"></post-library>
+        </template>
+        <template v-if="activeTab ==='JobClassification'" v-slot:JobClassification>
+          <job-classification v-loading="activeTab !=='JobClassification'"></job-classification>
+        </template>
+      </iep-tabs>
+    </basic-container>
+  </div>
 </template>
+<script>
+import PageHeader from '@/components/Page/Header'
+import IepTabs from '@/components/IepCommon/Tabs'
+import PostLibrary from './PostLibrary/'
+import JobClassification from './JobClassification/'
+export default {
+  components: { PageHeader, IepTabs, PostLibrary, JobClassification },
+  data () {
+    return {
+      replaceText: (data) => `（共有${data[0]}个岗位`,
+      tabList: [{
+        label: '岗位库',
+        value: 'PostLibrary',
+      }, {
+        label: '岗位分类',
+        value: 'JobClassification',
+      }],
+      activeTab: 'PostLibrary',
+    }
+  },
+}
+</script>

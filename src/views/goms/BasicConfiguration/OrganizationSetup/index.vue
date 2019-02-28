2b2c0ab36e87fd2ad29edc3f32ed252d90f68ca7
LuckyDu <1228522828@qq.com>
2019-02-28 13:47:03 +0800

    组织设置页面

--- src/views/goms/BasicConfiguration/OrganizationSetup/index.vue
+++ src/views/goms/BasicConfiguration/OrganizationSetup/index.vue
@@ -7,42 +7,56 @@
           <el-button size="small">组织变更记录</el-button>
         </template>
       </operation-container>
-      <el-form>
-        <div></div>
-        <div></div>
-        <div></div>
-        <el-form-item label="组织名称：" prop="name">
-          <el-input type="text" style="width:300px;"></el-input>
-        </el-form-item>
-        <el-form-item label="组织简称：" prop="orgAbrName">
-          <el-input style="width:300px;" placeholder="请输入组织简称"></el-input>
-        </el-form-item>
-        <el-form-item label="成立时间：" prop="establishTime">
-          <el-input style="width:300px;"></el-input>
-        </el-form-item>
-        <el-form-item label="创建人：" prop="creator">
-          <el-input style="width:300px;" type="text" disabled></el-input>
-        </el-form-item>
-        <el-form-item label="联系方式：" prop="contactMethod">
-          <el-input placeholder="请输入真实姓名"></el-input>
-        </el-form-item>
-        <el-form-item label="组织简介：" prop="intro">
-          <el-input type="textarea" placeholder="验证码登录使用"></el-input>
-        </el-form-item>
-        <el-form-item label="组织架构：" prop="orgStructure">
-          <el-input type="textarea"></el-input>
-        </el-form-item>
-        <el-form-item label="核心优势：" prop="coreAdvantage">
-          <el-input type="textarea" placeholder="请输入核心优势"></el-input>
-        </el-form-item>
-        <el-form-item label="组织客服人员：" prop="phone">
-          <el-input></el-input>
-        </el-form-item>
-        <el-form-item label="系统联系人员：" prop="phone">
-          <el-input></el-input>
-        </el-form-item>
-      </el-form>
-    </basic-container>
+      <div>
+        <div>
+          <el-button circle><i class="icon-diannaodenglu"></i><br>创建组织</el-button>
+        </div>
+        <div>
+          <el-button circle><i class="icon-organ"></i><br>创建联盟</el-button>
+        </div>
+        <div>
+          <el-button circle><i class="icon-chuangxinfuwu"></i><br>创建特殊组织</el-button>
+        </div>
+      </div>
+      <div>
+        <div>
+          <div slot="header" class="clearfix">
+            <span>组织升级</span>
+          </div>
+          <div class="text item">
+            组织创建者可将组织下属的一个部门分离为独立组织，默认子组织仍属于此创建人；转让后，该组织与母组织自动形成联盟，且通用母组织的各功能模块的标准配置。
+          </div>
+          <el-button type="warning" plain>升级</el-button>
+      </div>
+      <div>
+        <div slot="header" class="clearfix">
+          <span>组织合并</span>
+        </div>
+        <div class="text item">
+          联盟状态下，组织创建者可定向邀请联盟的兄弟组织进行合并；合并后的组织各项模块配置和文档均以发起组织为主，被合并组织的部分内容将被吞并。
+        </div>
+        <el-button type="warning" plain>合并</el-button>
+      </div>
+      <div>
+        <div slot="header" class="clearfix">
+          <span>组织转让</span>
+        </div>
+        <div class="text item">
+          组织创建者可将组织装让给组织现有的管理员之一；转让后，创建者自动退出组织管理。
+        </div>
+        <el-button type="warning" plain>转让</el-button>
+      </div>
+      <div>
+        <div slot="header" class="clearfix">
+          <span>组织解散</span>
+        </div>
+        <div class="text item">
+          组织创建者可解散组织；解散后，组织内的所有消息记录和成员信息都会删除，成员将无法进入，请谨慎操作。
+        </div>
+        <el-button type="warning" plain>解散</el-button>
+      </div>
+  </div>
+  </basic-container>
   </div>
 </template>
 <script>
@@ -56,3 +70,5 @@ export default {
   },
 }
 </script>
+<style lang="scss" scoped>
+</style>

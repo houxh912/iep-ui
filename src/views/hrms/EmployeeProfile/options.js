bd1454f6c36009743300fc29397e25fbfbc22fd2
张超 <zhangchao@126.com>
2019-03-07 15:46:37 +0800

    chore(src\views\hrms\EmployeeProfile\options.js): 员工字段太多了我总结了字段名字
    其他是修复了发布招聘遗漏的字段

--- src/views/hrms/EmployeeProfile/options.js
+++ src/views/hrms/EmployeeProfile/options.js
@@ -10,7 +10,51 @@ const dictsMap = {
     6: '离职员工',
   },
 }
-
+// const pageColumnObj = {
+//   id: '', // 用户ID
+//   name: '', // 姓名
+//   userName: '', // 用户名
+//   roleName: '', // 角色
+//   staffId: '', // 工号
+//   position: '', // 岗位
+//   externalTitle: '', // 对外头衔
+//   job: '', // 职务
+//   title: '', // 职称
+//   entryTime: '', // 入职时间
+//   positiveTime: '', // 转正时间
+//   transferTime: '', // 调动时间
+//   status: '', // 员工状态
+//   dept: '', // 所属部门
+//   birthday: '', // 出生年月
+//   sex: '', // 性别
+//   nationality: '', // 民族
+//   politicalStatus: '', // 政治面貌
+//   maritalStatus: '', // 婚姻状况
+//   fertilityStatus: '', // 生育状况
+//   language: '', // 外语水平
+//   education: '', // 最高学历
+//   graduatedSchool: '', // 毕业学校
+//   profession: '', // 专业
+//   graduationTime: '', // 毕业时间
+//   referrer: '', // 推荐人
+//   certifications: '', // 资质证书名称
+//   accountTypes: '', // 户口类别
+//   accountLocation: '', // 户口所在地
+//   residenceAddress: '', // 户籍地址
+//   currentAddress: '', // 现住地址
+//   IDCard: '', // 身份证
+//   phone: '', // 联系手机
+//   weChat: '', // 微信
+//   qq: '', // QQ
+//   email: '', // 邮箱
+//   emergencyName: '', // 应急联系人
+//   emergencyPhone: '', // 应急联系方式
+//   signingTime: '', // 劳动合同签订时间
+//   expirationTime: '', // 劳动合同到期时间
+//   benefitsStartTime: '', // 社保福利起缴时间
+//   benefitsStopTime: '', // 社保福利停止时间
+//   separationTime: '', // 离职时间
+// }
 const columnsMap = [
   {
     prop: 'sex',
@@ -42,7 +86,6 @@ const columnsMap = [
   {
     prop: 'idCard',
     label: '身份证号码',
-    minWidth: 120,
     hidden: false,
   },
 ]
@@ -83,26 +126,26 @@ const inittransferForm = () => {
 }
 const initdepartureForm = () => {
   return {
-    title:'',
-    departureTime:'',
-    reason:'',
+    title: '',
+    departureTime: '',
+    reason: '',
   }
 }
 const initinductionForm = () => {
   return {
-    name:'',
-    sex:'',
-    position:'',
-    idCard:'',
-    inductionTime:'',
+    name: '',
+    sex: '',
+    position: '',
+    idCard: '',
+    inductionTime: '',
   }
 }
 const initpositiveForm = () => {
   return {
-    name:'张三',
-    position:'产品',
-    psoitiveTime:'',
-    
+    name: '张三',
+    position: '产品',
+    psoitiveTime: '',
+
   }
 }
-export { dictsMap, columnsMap, initOrgForm, initSearchForm, inittransferForm,initdepartureForm,initinductionForm,initpositiveForm }
+export { dictsMap, columnsMap, initOrgForm, initSearchForm, inittransferForm, initdepartureForm, initinductionForm, initpositiveForm }
--- src/views/hrms/Recruitment/Publish/Page/Edit.vue
+++ src/views/hrms/Recruitment/Publish/Page/Edit.vue
@@ -17,7 +17,19 @@
         </el-row>
         <el-row>
           <el-col :span="12">
-            <el-form-item label="学历要求：" class="form-half">
+            <el-form-item label="招聘人数：">
+              <el-input-number v-model="form.recruitsCount"></el-input-number>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="目标人数：">
+              <el-input-number v-model="form.targetCount"></el-input-number>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="12">
+            <el-form-item label="学历要求：">
               <el-select v-model="form.academicId" placeholder="请选择">
                 <el-option v-for="item in dictGroup['hrms_highest_educational']" :key="item.value" :label="item.label" :value="item.value">
                 </el-option>
@@ -25,7 +37,7 @@
             </el-form-item>
           </el-col>
           <el-col :span="12">
-            <el-form-item label="工作类型：" class="form-half">
+            <el-form-item label="工作类型：">
               <el-select v-model="form.jobTypeId" placeholder="请选择">
                 <el-option v-for="item in dictGroup['hrms_work_type']" :key="item.value" :label="item.label" :value="item.value">
                 </el-option>
@@ -35,12 +47,24 @@
         </el-row>
         <el-row>
           <el-col :span="12">
-            <el-form-item label="工作地点：" class="form-half">
+            <el-form-item label="工作年限：">
+              <el-input-number v-model="form.years"></el-input-number>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="专业要求：">
+              <el-input v-model="form.profession"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="12">
+            <el-form-item label="工作地点：">
               <el-input v-model="form.place"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="12">
-            <el-form-item label="性别：" class="form-half">
+            <el-form-item label="性别：">
               <el-radio-group v-model="form.sex">
                 <el-radio :label="1">男</el-radio>
                 <el-radio :label="2">女</el-radio>
@@ -50,25 +74,25 @@
         </el-row>
         <el-row>
           <el-col :span="12">
-            <el-form-item label="工资待遇：" class="form-half">
+            <el-form-item label="工资待遇：">
               <el-input v-model="form.treatment"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="12">
-            <el-form-item label="外语要求：" class="form-half">
+            <el-form-item label="外语要求：">
               <el-input v-model="form.language"></el-input>
             </el-form-item>
           </el-col>
         </el-row>
         <el-row>
           <el-col :span="12">
-            <el-form-item label="招聘期限：" class="form-half">
+            <el-form-item label="招聘期限：">
               <el-date-picker v-model="form.term" type="date" placeholder="选择日期">
               </el-date-picker>
             </el-form-item>
           </el-col>
           <el-col :span="12">
-            <el-form-item label="福利待遇：" class="form-half">
+            <el-form-item label="福利待遇：">
               <el-input v-model="form.welfare"></el-input>
             </el-form-item>
           </el-col>

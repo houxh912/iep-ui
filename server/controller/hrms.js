91ebfcf28f8eca03af43e5c37975da3fdd20d5e4
张超 <zhangchao@126.com>
2019-03-13 17:30:56 +0800

    chore(server\router.js): ADD 人力模块基本功能的假数据, 完成人才发布的批量删与单个删

--- server/controller/hrms.js
+++ server/controller/hrms.js
@@ -120,10 +120,10 @@ exports.getAdministrativeApprovalPage = async function (ctx) {
   ctx.body = data
 }
 exports.getPublishRecruitmentPage = async function (ctx) {
-  ctx.body = `{"code":0,"msg":"success","data":{"records":[{"id":9,"position":"产品经理","dept":"山东农信","recruitsCount":2,"jobType":"全职","applicationTime":"2019-03-07 09:58:33","status":3},{"id":5,"position":"产品总监","dept":"院校农信2","recruitsCount":17,"jobType":"全职/兼职","applicationTime":"2019-03-07 09:58:33","status":3},{"id":1,"position":"数据元专员","dept":"院校农信1","recruitsCount":14,"jobType":"全职","applicationTime":"2019-03-07 10:02:12","status":2},{"id":2,"position":"产品经理","dept":"院校农信2","recruitsCount":15,"jobType":"兼职","applicationTime":"2019-03-06 14:14:22","status":2}],"total":4,"size":10,"current":1,"searchCount":true,"pages":1}}`
+  ctx.body = `{"code":0,"msg":"success","data":{"records":[{"id":25,"position":"产品总监","dept":"杜照鸿的组织","recruitsCount":1,"jobType":"全职","applicationTime":"2019-03-12 15:58:11","status":1},{"id":24,"position":null,"dept":"产品部","recruitsCount":0,"jobType":"全职","applicationTime":"2019-03-12 15:58:01","status":1},{"id":23,"position":null,"dept":"54545454","recruitsCount":0,"jobType":"全职","applicationTime":"2019-03-12 15:57:45","status":1},{"id":22,"position":"产品助理","dept":"saas","recruitsCount":0,"jobType":"全职","applicationTime":"2019-03-12 15:56:56","status":1},{"id":21,"position":null,"dept":"比你嘛","recruitsCount":1,"jobType":"全职/兼职","applicationTime":"2019-03-12 13:50:44","status":3},{"id":19,"position":"产品总监","dept":"产品部","recruitsCount":1,"jobType":"兼职","applicationTime":"2019-03-08 10:33:39","status":1},{"id":15,"position":"产品总监","dept":"测试添加0311部门","recruitsCount":0,"jobType":"兼职","applicationTime":"2019-03-07 15:15:15","status":1},{"id":14,"position":"产品总监","dept":"测试新增组织并绑定所创大部门","recruitsCount":1,"jobType":"兼职","applicationTime":"2019-03-07 15:39:55","status":2},{"id":13,"position":null,"dept":"产品部","recruitsCount":0,"jobType":"兼职","applicationTime":"2019-03-07 14:24:27","status":3},{"id":8,"position":"产品经理","dept":"sasasa","recruitsCount":35,"jobType":"全职","applicationTime":"2019-03-06 14:14:22","status":2}],"total":15,"size":10,"current":1,"searchCount":true,"pages":2}}`
 }
 exports.getPublishRecruitmentById = async function (ctx) {
-  ctx.body = `{"code":0,"msg":"success","data":{"id":9,"positionId":2,"positionName":"产品经理","deptId":1,"deptName":"山东农信","language":"dsfdsf","recruitsCount":2,"targetCount":2,"academicId":1,"jobTypeId":1,"years":2,"profession":"sddsfsdf","place":"sdsfsdf","sex":2,"treatment":"sdfsdf","term":"2019-03-26T16:00:00.000Z","welfare":"sdfdsfds","claim":"sdfsdfdsfdsfsdfsdf","duties":"sdffsddfs"}}`
+  ctx.body = `{"code":0,"msg":"success","data":{"id":25,"positionId":1,"deptId":19,"language":"三","positionClsId":1,"position":[1,1],"parentDept":0,"dept":[19],"recruitsCount":1,"targetCount":1,"academicId":2,"jobTypeId":1,"years":1,"profession":"要求","place":"科学城","sex":1,"treatment":"面议","term":"2019-03-14T16:00:00.000Z","welfare":"三","claim":"string","duties":"string"}}`
 }
 exports.commonPublishRecruitment = async function (ctx) {
   ctx.body = `{"code":0,"msg":"success","data":true}`
@@ -188,69 +188,7 @@ exports.getTrainingRecordPage = async function (ctx) {
   ctx.body = data
 }
 exports.getDepartmentManagePage = async function (ctx) {
-  const data = {
-    code: 0,
-    msg: 'success',
-    data: {
-      records: [
-        {
-          id: 1,
-          部门名称: '技术部',
-          负责人: '111',
-          部门人数: '1',
-          成立时间: '2019-03-01'
-        },
-        {
-          id: 2,
-          部门名称: '技术部',
-          负责人: '111',
-          部门人数: '1',
-          成立时间: '2019-03-01'
-        },
-        {
-          id: 3,
-          部门名称: '技术部',
-          负责人: '111',
-          部门人数: '1',
-          成立时间: '2019-03-01'
-        },
-        {
-          id: 4,
-          部门名称: '技术部',
-          负责人: '111',
-          部门人数: '1',
-          成立时间: '2019-03-01'
-        },
-        {
-          id: 5,
-          部门名称: '技术部',
-          负责人: '111',
-          部门人数: '1',
-          成立时间: '2019-03-01'
-        },
-        {
-          id: 6,
-          部门名称: '技术部',
-          负责人: '111',
-          部门人数: '1',
-          成立时间: '2019-03-01'
-        },
-        {
-          id: 7,
-          部门名称: '技术部',
-          负责人: '111',
-          部门人数: '1',
-          成立时间: '2019-03-01'
-        }
-      ],
-      total: 6,
-      size: 10,
-      current: 1,
-      searchCount: true,
-      pages: 1
-    }
-  }
-  ctx.body = data
+  ctx.body = `{"code":0,"msg":"success","data":{"records":[{"id":33,"name":"产品部不","parentId":16,"number":"565656","userName":"杜照鸿","userId":10,"orgId":5,"people":0,"establishedTime":"2019-03-14 00:00:00","children":[]},{"id":32,"name":"产品部","parentId":16,"number":"1311","userName":"杜照鸿","userId":10,"orgId":5,"people":0,"establishedTime":"2019-03-13 00:00:00","children":[]},{"id":29,"name":"saas","parentId":16,"number":"saa","userName":"超级管理员","userId":1,"orgId":5,"people":0,"establishedTime":"2019-03-20 00:00:00","children":[]}],"total":3,"size":10,"current":1,"searchCount":true,"pages":1}}`
 }
 exports.getJobPage = async function (ctx) {
   const data = {
@@ -393,4 +331,7 @@ exports.getPostTypePage = async function (ctx) {
     }
   }
   ctx.body = data
+}
+exports.getPostTypeList = async function (ctx) {
+  ctx.body = `{"code":0,"msg":"success","data":[{"value":1,"label":"产品技术类","children":[{"value":1,"label":"产品总监","children":null},{"value":2,"label":"产品经理","children":null},{"value":4,"label":"产品助理","children":null},{"value":5,"label":"数据元专员","children":null}]},{"value":8,"label":"研发技术类","children":[{"value":12,"label":"产品经理","children":null}]},{"value":10,"label":"行政管理类","children":[]},{"value":13,"label":"string","children":[]},{"value":14,"label":"string","children":[]}]}`
 }

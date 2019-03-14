37c90420a9a6205340f7185ef6603519d7a46afc
张超 <zhangchao@126.com>
2019-03-14 21:00:03 +0800

    feat(src\page\index\top\TopGuide.vue): 修复消息组件的互动问题, 添加使用指南图标

--- src/page/index/top/TopMessage.vue
+++ src/page/index/top/TopMessage.vue
@@ -1,58 +1,58 @@
 <template>
   <div class="nav-info">
-    <el-badge :value="12" class="item">
-      <iep-button plain @click="visible = !visible"><i class="iconfont icon-xiaoxi2"></i></iep-button>
-      <el-popover placement="bottom" width="336" trigger="manual" v-model="visible">
-        <el-tabs v-model="activeName" @tab-click="handleClick">
-          <el-tab-pane label="通知 (4)" name="first">
-            <el-card class="box-card">
-              <div v-for="msg in msgList" :key="msg.id" class="text">
-                <div class="list-item-icon">
-                  <img :src="msg.img" alt="" />
-                </div>
-                <div class="list-item-content">
-                  <h4 class="list-item-title">{{ msg.title }}</h4>
-                  <div class="list-item-description">
-                    <span class="time">{{ msg.time }}</span>
-                  </div>
+    <el-popover popper-class="msg-popover" placement="bottom" width="336" trigger="click">
+      <el-tabs class="msg-tabs" v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="通知 (4)" name="first">
+          <el-card class="box-card">
+            <div v-for="msg in msgList" :key="msg.id" class="text">
+              <div class="list-item-icon">
+                <img :src="msg.img" alt="" />
+              </div>
+              <div class="list-item-content">
+                <h4 class="list-item-title">{{ msg.title }}</h4>
+                <div class="list-item-description">
+                  <span class="time">{{ msg.time }}</span>
                 </div>
               </div>
-              <div class="list-loadMore">加载更多</div>
-            </el-card>
-          </el-tab-pane>
-          <el-tab-pane label="消息 (5)" name="second">
-            <el-card class="box-card">
-              <div v-for="info in infoList" :key="info.id" class="text">
-                <div class="list-item-content">
-                  <h4 class="list-item-title">{{ info.title }}</h4>
-                  <div class="list-item-description">
-                    <span class="time">{{ info.time }}</span>
-                  </div>
+            </div>
+            <div class="list-loadMore">加载更多</div>
+          </el-card>
+        </el-tab-pane>
+        <el-tab-pane label="消息 (5)" name="second">
+          <el-card class="box-card">
+            <div v-for="info in infoList" :key="info.id" class="text">
+              <div class="list-item-content">
+                <h4 class="list-item-title">{{ info.title }}</h4>
+                <div class="list-item-description">
+                  <span class="time">{{ info.time }}</span>
                 </div>
               </div>
-              <div class="list-loadMore">加载更多</div>
-            </el-card>
-          </el-tab-pane>
-          <el-tab-pane label="邮件 (3)" name="third">
-            <el-card class="box-card">
-              <div v-for="mail in mailList" :key="mail.id" class="text">
-                <div class="list-item-icon">
-                  <i class="iconfont icon-youxiang"></i>
-                </div>
-                <div class="list-item-content">
-                  <h4 class="list-item-title">{{ mail.title }}</h4>
-                  <div class="list-item-description">
-                    <span class="time">{{ mail.time }}</span>
-                  </div>
+            </div>
+            <div class="list-loadMore">加载更多</div>
+          </el-card>
+        </el-tab-pane>
+        <el-tab-pane label="邮件 (3)" name="third">
+          <el-card class="box-card">
+            <div v-for="mail in mailList" :key="mail.id" class="text">
+              <div class="list-item-icon">
+                <i class="iconfont icon-youxiang"></i>
+              </div>
+              <div class="list-item-content">
+                <h4 class="list-item-title">{{ mail.title }}</h4>
+                <div class="list-item-description">
+                  <span class="time">{{ mail.time }}</span>
                 </div>
               </div>
-              <div class="list-loadMore">加载更多</div>
-            </el-card>
-          </el-tab-pane>
-        </el-tabs>
-        <div class="list-clear">清空 通知</div>
-      </el-popover>
-    </el-badge>
+            </div>
+            <div class="list-loadMore">加载更多</div>
+          </el-card>
+        </el-tab-pane>
+      </el-tabs>
+      <div class="list-clear">清空 通知</div>
+      <el-badge :value="12" slot="reference" class="item">
+        <iep-button><i class="el-icon-bell"></i></iep-button>
+      </el-badge>
+    </el-popover>
   </div>
 </template>
 <script>
@@ -140,132 +140,129 @@ export default {
   },
 }
 </script>
-
+<style lang="css" scoped>
+.item >>> .el-badge__content.is-fixed {
+  top: 20px;
+  right: 22px;
+}
+.msg-popover >>> .el-popover {
+  right: 0;
+}
+.msg-tabs >>> .el-tabs__nav {
+  width: 100%;
+  text-align: center;
+}
+.msg-tabs >>> .el-tabs__active-bar {
+  left: 40px;
+  width: 70px !important;
+}
+.msg-tabs >>> .el-card__body {
+  padding: 0 !important;
+}
+.msg-tabs >>> .el-tabs__header {
+  margin-bottom: 0;
+}
+.msg-tabs >>> .el-card__body {
+  height: 360px;
+  overflow: auto;
+}
+.msg-tabs >>> ::-webkit-scrollbar {
+  border-radius: 10px;
+  width: 8px;
+  background-color: #fff;
+}
+.msg-tabs >>> ::-webkit-scrollbar-track {
+  border-radius: 10px;
+  background-color: #fff;
+  -webkit-transition: 0.3s background-color;
+  transition: 0.3s background-color;
+}
+.msg-tabs >>> ::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  background-color: #ccc;
+  -webkit-transition: 0.3s background-color;
+  transition: 0.3s background-color;
+}
+</style>
 <style lang="scss" scoped>
 h4 {
   margin: 0;
   padding: 0;
   font-weight: 400;
 }
-.nav-info {
-  .item {
-    position: relative;
-    margin-right: 20px;
-    button {
-      border: 0;
-      height: 60px;
-      i {
-        font-size: 20px;
-      }
-      &:focus,
-      &:hover {
-        background-color: #eee;
-        color: #333;
-      }
+.item {
+  position: relative;
+  margin-right: 20px;
+  button {
+    border: 0;
+    height: 60px;
+    i {
+      font-size: 20px;
     }
-    .list-clear {
-      height: 46px;
-      line-height: 46px;
-      text-align: center;
-      border-top: 1px solid #eceef5;
-      color: #999;
-      background-color: #fff;
-      cursor: pointer;
-      transition: all 0.3s;
-      &:hover {
-        color: #666;
-      }
-    }
-    .el-card {
-      border: 0;
-      .text {
-        display: flex;
-        flex: 1 1;
-        align-items: flex-start;
-        padding: 20px;
-        border-bottom: 1px solid #eceef5;
-        .list-item-icon {
-          margin-top: 5px;
-          margin-right: 16px;
-          width: 32px;
-          height: 32px;
-          img {
-            display: block;
-            width: 100%;
-          }
-          i {
-            font-size: 20px;
-          }
-        }
-        .list-item-content {
-          flex: 1 0;
-          .list-item-title {
-            margin-bottom: 4px;
-            color: rgba(0, 0, 0, 0.65);
-            font-size: 14px;
-            line-height: 22px;
-          }
-          .list-item-description {
-            color: #999;
-            font-size: 12px;
-            line-height: 22px;
-          }
-        }
-      }
-      .list-loadMore {
-        padding: 8px 0;
-        color: #409eff;
-        text-align: center;
-        cursor: pointer;
-        &:hover {
-          color: #66b1ff;
-        }
-      }
+    &:focus,
+    &:hover {
+      background-color: #eee;
+      color: #333;
     }
   }
 }
-</style>
-<style scoped>
-.nav-info >>> .el-badge__content.is-fixed {
-  top: 20px;
-  right: 22px;
-}
-.nav-info >>> .el-popover {
-  right: 0;
-}
-.nav-info >>> .el-tabs__nav {
-  width: 100%;
+.list-clear {
+  height: 46px;
+  line-height: 46px;
   text-align: center;
-}
-.nav-info >>> .el-tabs__active-bar {
-  left: 40px;
-  width: 70px !important;
-}
-.nav-info >>> .el-card__body {
-  padding: 0 !important;
-}
-.nav-info >>> .el-tabs__header {
-  margin-bottom: 0;
-}
-.nav-info >>> .el-card__body {
-  height: 360px;
-  overflow: auto;
-}
-.nav-info >>> ::-webkit-scrollbar {
-  border-radius: 10px;
-  width: 8px;
+  border-top: 1px solid #eceef5;
+  color: #999;
   background-color: #fff;
+  cursor: pointer;
+  transition: all 0.3s;
+  &:hover {
+    color: #666;
+  }
 }
-.nav-info >>> ::-webkit-scrollbar-track {
-  border-radius: 10px;
-  background-color: #fff;
-  -webkit-transition: 0.3s background-color;
-  transition: 0.3s background-color;
-}
-.nav-info >>> ::-webkit-scrollbar-thumb {
-  border-radius: 10px;
-  background-color: #ccc;
-  -webkit-transition: 0.3s background-color;
-  transition: 0.3s background-color;
+.box-card {
+  border: 0;
+  .text {
+    display: flex;
+    flex: 1 1;
+    align-items: flex-start;
+    padding: 20px;
+    border-bottom: 1px solid #eceef5;
+    .list-item-icon {
+      margin-top: 5px;
+      margin-right: 16px;
+      width: 32px;
+      height: 32px;
+      img {
+        display: block;
+        width: 100%;
+      }
+      i {
+        font-size: 20px;
+      }
+    }
+    .list-item-content {
+      flex: 1 0;
+      .list-item-title {
+        margin-bottom: 4px;
+        color: rgba(0, 0, 0, 0.65);
+        font-size: 14px;
+        line-height: 22px;
+      }
+      .list-item-description {
+        color: #999;
+        font-size: 12px;
+        line-height: 22px;
+      }
+    }
+  }
+  .list-loadMore {
+    padding: 8px 0;
+    color: #409eff;
+    text-align: center;
+    cursor: pointer;
+    &:hover {
+      color: #66b1ff;
+    }
+  }
 }
 </style>

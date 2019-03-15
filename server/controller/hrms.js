b541ee1bca00606c981a6bb431a03dca2d4b47c6
张超 <zhangchao@126.com>
2019-03-15 10:01:26 +0800

    chore(server\controller\hrms.js): 完善人力分页的最新假数据

--- server/controller/hrms.js
+++ server/controller/hrms.js
@@ -120,7 +120,7 @@ exports.getAdministrativeApprovalPage = async function (ctx) {
   ctx.body = data
 }
 exports.getPublishRecruitmentPage = async function (ctx) {
-  ctx.body = `{"code":0,"msg":"success","data":{"records":[{"id":25,"position":"产品总监","dept":"杜照鸿的组织","recruitsCount":1,"jobType":"全职","applicationTime":"2019-03-12 15:58:11","status":1},{"id":24,"position":null,"dept":"产品部","recruitsCount":0,"jobType":"全职","applicationTime":"2019-03-12 15:58:01","status":1},{"id":23,"position":null,"dept":"54545454","recruitsCount":0,"jobType":"全职","applicationTime":"2019-03-12 15:57:45","status":1},{"id":22,"position":"产品助理","dept":"saas","recruitsCount":0,"jobType":"全职","applicationTime":"2019-03-12 15:56:56","status":1},{"id":21,"position":null,"dept":"比你嘛","recruitsCount":1,"jobType":"全职/兼职","applicationTime":"2019-03-12 13:50:44","status":3},{"id":19,"position":"产品总监","dept":"产品部","recruitsCount":1,"jobType":"兼职","applicationTime":"2019-03-08 10:33:39","status":1},{"id":15,"position":"产品总监","dept":"测试添加0311部门","recruitsCount":0,"jobType":"兼职","applicationTime":"2019-03-07 15:15:15","status":1},{"id":14,"position":"产品总监","dept":"测试新增组织并绑定所创大部门","recruitsCount":1,"jobType":"兼职","applicationTime":"2019-03-07 15:39:55","status":2},{"id":13,"position":null,"dept":"产品部","recruitsCount":0,"jobType":"兼职","applicationTime":"2019-03-07 14:24:27","status":3},{"id":8,"position":"产品经理","dept":"sasasa","recruitsCount":35,"jobType":"全职","applicationTime":"2019-03-06 14:14:22","status":2}],"total":15,"size":10,"current":1,"searchCount":true,"pages":2}}`
+  ctx.body = `{"code":0,"msg":"success","data":{"records":[{"id":26,"position":"数据元专员","dept":"技术部分部1","recruitsCount":2,"jobType":"全职/兼职","applicationTime":"2019-03-14 15:20:44","status":1},{"id":25,"position":"产品经理","dept":"产品部","recruitsCount":5,"jobType":"全职/兼职","applicationTime":"2019-03-14 13:51:01","status":1},{"id":22,"position":"产品助理","dept":"技术部","recruitsCount":0,"jobType":"全职","applicationTime":"2019-03-12 15:56:56","status":1},{"id":19,"position":"产品总监","dept":"产品部","recruitsCount":1,"jobType":"兼职","applicationTime":"2019-03-14 13:56:39","status":2},{"id":15,"position":"产品总监","dept":"测试添加0311部门","recruitsCount":0,"jobType":"兼职","applicationTime":"2019-03-07 15:15:15","status":1},{"id":14,"position":"产品总监","dept":"测试新增组织并绑定所创大部门","recruitsCount":1,"jobType":"兼职","applicationTime":"2019-03-07 15:39:55","status":2},{"id":8,"position":"产品经理","dept":"sasasa","recruitsCount":35,"jobType":"全职","applicationTime":"2019-03-06 14:14:22","status":2},{"id":7,"position":"产品助理","dept":"测试新增组织并绑定所创大部门","recruitsCount":52,"jobType":"全职","applicationTime":"2019-03-06 14:14:22","status":3},{"id":1,"position":"数据元专员","dept":"杜照鸿的组织","recruitsCount":14,"jobType":"全职","applicationTime":"2019-03-07 10:02:12","status":3},{"id":2,"position":"产品经理","dept":"测试添加0311部门","recruitsCount":15,"jobType":"兼职","applicationTime":"2019-03-06 14:14:22","status":3}],"total":10,"size":10,"current":1,"searchCount":true,"pages":1}}`
 }
 exports.getPublishRecruitmentById = async function (ctx) {
   ctx.body = `{"code":0,"msg":"success","data":{"id":25,"positionId":1,"deptId":19,"language":"三","positionClsId":1,"position":[1,1],"parentDept":0,"dept":[19],"recruitsCount":1,"targetCount":1,"academicId":2,"jobTypeId":1,"years":1,"profession":"要求","place":"科学城","sex":1,"treatment":"面议","term":"2019-03-14T16:00:00.000Z","welfare":"三","claim":"string","duties":"string"}}`
@@ -188,149 +188,19 @@ exports.getTrainingRecordPage = async function (ctx) {
   ctx.body = data
 }
 exports.getDepartmentManagePage = async function (ctx) {
-  ctx.body = `{"code":0,"msg":"success","data":{"records":[{"id":33,"name":"产品部不","parentId":16,"number":"565656","userName":"杜照鸿","userId":10,"orgId":5,"people":0,"establishedTime":"2019-03-14 00:00:00","children":[]},{"id":32,"name":"产品部","parentId":16,"number":"1311","userName":"杜照鸿","userId":10,"orgId":5,"people":0,"establishedTime":"2019-03-13 00:00:00","children":[]},{"id":29,"name":"saas","parentId":16,"number":"saa","userName":"超级管理员","userId":1,"orgId":5,"people":0,"establishedTime":"2019-03-20 00:00:00","children":[]}],"total":3,"size":10,"current":1,"searchCount":true,"pages":1}}`
+  ctx.body = `{"code":0,"msg":"success","data":{"records":[{"id":33,"name":"产品部不","parentId":16,"number":"565656","userName":"杜照鸿","userId":10,"orgId":5,"people":0,"establishedTime":"2019-03-14 00:00:00","children":[]},{"id":32,"name":"产品部","parentId":16,"number":"1311","userName":"杜照鸿","userId":10,"orgId":5,"people":0,"establishedTime":"2019-03-13 00:00:00","children":[]},{"id":29,"name":"技术部","parentId":16,"number":"NO1127","userName":"杜照鸿","userId":10,"orgId":5,"people":0,"establishedTime":"2019-03-20 00:00:00","children":[{"id":35,"name":"技术部分部2","parentId":29,"parentName":"技术部","number":"NO11272","userName":"杜照鸿","userId":10,"orgId":5,"people":0,"establishedTime":"2019-03-14 00:00:00"},{"id":34,"name":"技术部分部1","parentId":29,"parentName":"技术部","number":"NO11271","userName":"超级管理员","userId":1,"orgId":5,"people":0,"establishedTime":"2019-03-14 00:00:00"}]}],"total":3,"size":10,"current":1,"searchCount":true,"pages":1}}`
 }
 exports.getJobPage = async function (ctx) {
-  const data = {
-    code: 0,
-    msg: 'success',
-    data: {
-      records: [
-        {
-          id: 1,
-          name: '专员',
-          description: '职务说明',
-          priority: '1'
-        },
-      ],
-      total: 10,
-      size: 10,
-      current: 1,
-      searchCount: true,
-      pages: 1
-    }
-  }
-  ctx.body = data
+  ctx.body = `{"code":0,"msg":"success","data":{"records":[{"id":5,"name":"专员","priority":0,"description":"专员"},{"id":6,"name":"初级主管","priority":0,"description":"初级主管"},{"id":7,"name":"中级主管","priority":88,"description":"中级主管"},{"id":8,"name":"中级主管","priority":3,"description":"中级主管"},{"id":9,"name":"中级主管","priority":0,"description":"中级主管"},{"id":10,"name":"高级主管","priority":0,"description":"高级主管"},{"id":11,"name":"初级主管","priority":0,"description":"初级主管"},{"id":12,"name":"专员","priority":0,"description":"专员"},{"id":13,"name":"董事长","priority":0,"description":"董事长"},{"id":14,"name":"总经理","priority":0,"description":"总经理"}],"total":19,"size":10,"current":1,"searchCount":true,"pages":2}}`
 }
 exports.getTitlePage = async function (ctx) {
-  const data = {
-    code: 0,
-    msg: 'success',
-    data: {
-      records: [
-        {
-          id: 1,
-          name: '专员',
-          description: '职称说明',
-          priority: '1'
-        },
-      ],
-      total: 6,
-      size: 10,
-      current: 1,
-      searchCount: true,
-      pages: 1
-    }
-  }
-  ctx.body = data
+  ctx.body = `{"code":0,"msg":"success","data":{"records":[{"id":1,"name":"经理助理","priority":6,"description":"助理经理"},{"id":2,"name":"职业助理","priority":9,"description":"职业助理"},{"id":10,"name":"首席咨询师","priority":0,"description":"首席咨询师"},{"id":11,"name":"资深咨询师","priority":0,"description":"资深咨询师"},{"id":12,"name":"战略咨询师","priority":0,"description":"战略咨询师"},{"id":13,"name":"高级咨询师","priority":0,"description":"高级咨询师"},{"id":14,"name":"中级咨询师","priority":0,"description":"中级咨询师"},{"id":15,"name":"初级咨询师","priority":0,"description":"初级咨询师"},{"id":16,"name":"助理咨询师","priority":0,"description":"助理咨询师"},{"id":17,"name":"数据分析员","priority":0,"description":"数据分析员"}],"total":11,"size":10,"current":1,"searchCount":true,"pages":2}}`
 }
 exports.getPostLibraryPage = async function (ctx) {
-  const data = {
-    code: 0,
-    msg: 'success',
-    data: {
-      records: [
-        {
-          id: 1,
-          type: '产品技术类',
-          name: '产品经理',
-          count: '1'
-        },
-        {
-          id: 2,
-          type: '产品技术类',
-          name: '产品经理',
-          count: '10'
-        },
-        {
-          id: 3,
-          type: '产品技术类',
-          name: '产品经理',
-          count: '100'
-        },
-        {
-          id: 4,
-          type: '产品技术类',
-          name: '产品经理',
-          count: '1000'
-        },
-        {
-          id: 5,
-          type: '产品技术类',
-          name: '产品经理',
-          count: '1'
-        },
-        {
-          id: 6,
-          type: '产品技术类',
-          name: '产品经理',
-          count: '1'
-        }
-      ],
-      total: 6,
-      size: 10,
-      current: 1,
-      searchCount: true,
-      pages: 1
-    }
-  }
-  ctx.body = data
+  ctx.body = `{"code":0,"msg":"success","data":{"records":[{"count":0,"id":1,"typeId":1,"name":"产品总监","typeName":"产品技术类","duties":"做好产品","claim":"产品技术顶尖"},{"count":4,"id":2,"typeId":1,"name":"产品经理","typeName":"产品技术类","duties":"做好产品业务","claim":"协助产品总监"},{"count":0,"id":4,"typeId":1,"name":"产品助理","typeName":"产品技术类","duties":"帮助设计","claim":"能够帮助分担"},{"count":0,"id":5,"typeId":1,"name":"数据元专员","typeName":"产品技术类","duties":"整理数据111","claim":"整理好数据111"},{"count":0,"id":12,"typeId":8,"name":"产品经理","typeName":"研发技术类","duties":"负责产品技术","claim":"精通产品技术"}],"total":5,"size":10,"current":1,"searchCount":true,"pages":1}}`
 }
 exports.getPostTypePage = async function (ctx) {
-  const data = {
-    code: 0,
-    msg: 'success',
-    data: {
-      records: [
-        {
-          id: 1,
-          name: '产品技术类',
-          description: '产品技术类说明'
-        },
-        {
-          id: 2,
-          name: '产品技术类',
-          description: '产品技术类说明'
-        },
-        {
-          id: 3,
-          name: '产品技术类',
-          description: '产品技术类说明'
-        },
-        {
-          id: 4,
-          name: '产品技术类',
-          description: '产品技术类说明'
-        },
-        {
-          id: 5,
-          name: '产品技术类',
-          description: '产品技术类说明'
-        },
-        {
-          id: 6,
-          name: '产品技术类',
-          description: '产品技术类说明'
-        }
-      ],
-      total: 6,
-      size: 10,
-      current: 1,
-      searchCount: true,
-      pages: 1
-    }
-  }
-  ctx.body = data
+  ctx.body = `{"code":0,"msg":"success","data":{"records":[{"id":1,"name":"产品技术类","description":"包括各种产品和技术类的工作1"},{"id":8,"name":"研发技术类","description":"包括研发微服务开发工具"},{"id":10,"name":"行政管理类","description":"行政管理类"}],"total":3,"size":10,"current":1,"searchCount":true,"pages":1}}`
 }
 exports.getPostTypeList = async function (ctx) {
   ctx.body = `{"code":0,"msg":"success","data":[{"value":1,"label":"产品技术类","children":[{"value":1,"label":"产品总监","children":null},{"value":2,"label":"产品经理","children":null},{"value":4,"label":"产品助理","children":null},{"value":5,"label":"数据元专员","children":null}]},{"value":8,"label":"研发技术类","children":[{"value":12,"label":"产品经理","children":null}]},{"value":10,"label":"行政管理类","children":[]},{"value":13,"label":"string","children":[]},{"value":14,"label":"string","children":[]}]}`

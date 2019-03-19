3fa10c83a63763d6c2a33cb89860f7544b64d2fe
张超 <zhangchao@126.com>
2019-03-19 23:18:14 +0800

    feat(src\components\IepContact\Multiple.vue): ADD多选通讯录

--- src/components/IepContact/Multiple.vue
+++ src/components/IepContact/Multiple.vue
@@ -1,12 +1,20 @@
 <template>
-  <el-input v-model="user.name" placeholder="请输入选择用户" readonly>
-    <el-popover slot="append" placement="right" width="300" trigger="click" v-model="dialogShow">
-      <el-tree :props="props" :load="loadNode" lazy :show-checkbox="showCheckbox" @node-click="selectUser"></el-tree>
+  <div class="multiple-box">
+    <el-tag type="danger" closable v-for="tag in unions" :key="tag.id" @close="handleClose(tag, 'unions')">{{tag.name}}</el-tag>
+    <el-tag type="warning" closable v-for="tag in orgs" :key="tag.id" @close="handleClose(tag, 'orgs')">{{tag.name}}</el-tag>
+    <el-tag type="success" closable v-for="tag in users" :key="tag.id" @close="handleClose(tag, 'users')">{{tag.name}}</el-tag>
+    <el-popover placement="right" width="300" trigger="click" v-model="dialogShow">
+      <el-tree ref="tree" :props="props" :load="loadNode" lazy :show-checkbox="showCheckbox" @node-click="selectGroup" :expand-on-click-node="false" :filter-node-method="filterNodeMethod"></el-tree>
       <iep-button slot="reference">选择</iep-button>
     </el-popover>
-  </el-input>
+  </div>
 </template>
 <script>
+// receivers: { // 比如接收人
+//     unions: [{id: 1, name: '国脉'}],
+//     orgs: [{id: 2, name: '技术中心'}],
+//     users: [{id: 3, name: '张超'}],
+//   }, 
 import { getUnionList, getOrgListById, getUserListById } from '@/api/admin/contacts'
 export default {
   props: {
@@ -28,23 +36,76 @@ export default {
     }
   },
   computed: {
-    user: {
-      // getter
-      get: function () {
-        return this.value
+    users: {
+      get: function () { return this.group.users },
+      set: function (value) { this.group.users = value },
+    },
+    userIds: function () { return this.group.users.map(m => m.id) },
+    orgs: {
+      get: function () { return this.group.orgs },
+      set: function (value) { this.group.orgs = value },
+    },
+    orgIds: function () { return this.group.orgs.map(m => m.id) },
+    unions: {
+      get: function () { return this.group.unions },
+      set: function (value) { this.group.unions = value },
+    },
+    unionIds: function () { return this.group.unions.map(m => m.id) },
+    group: {
+      get: function () { return this.value },
+      set: function (value) { this.$emit('input', value) },
+    },
+  },
+  watch: {
+    group: {
+      handler: function (newVal) {
+        this.$refs.tree.filter(newVal)
       },
-      // setter
-      set: function (value) {
-        this.$emit('input', value)
-      },
+      deep: true,
     },
   },
   methods: {
-    selectUser (data, node) {
+    handleClose (tag, arr) {
+      const newData = this.group[arr].filter(item => item.id !== tag.id)
+      this.group[arr] = newData
+    },
+    filterNodeMethod (value, data, node) {
+      if (node.level === 1 && this.unionIds.includes(data.value)) {
+        return false
+      }
+      if (node.level === 2 && this.orgIds.includes(data.value)) {
+        return false
+      }
+      if (node.level === 3 && this.userIds.includes(data.value)) {
+        return false
+      }
+      return true
+    },
+    selectGroup (data, node) {
       if (node.level === 3) {
-        this.user = {
-          id: data.value,
-          name: data.label,
+        if (!this.userIds.includes(data.value)) {
+          this.users.push({
+            id: data.value,
+            name: data.label,
+          })
+        }
+        this.dialogShow = false
+      }
+      if (node.level === 2) {
+        if (!this.orgIds.includes(data.value)) {
+          this.orgs.push({
+            id: data.value,
+            name: data.label,
+          })
+        }
+        this.dialogShow = false
+      }
+      if (node.level === 1) {
+        if (!this.unionIds.includes(data.value)) {
+          this.unions.push({
+            id: data.value,
+            name: data.label,
+          })
         }
         this.dialogShow = false
       }
@@ -69,3 +130,8 @@ export default {
   },
 }
 </script>
+<style scoped>
+.multiple-box > .el-tag {
+  margin-right: 5px;
+}
+</style>

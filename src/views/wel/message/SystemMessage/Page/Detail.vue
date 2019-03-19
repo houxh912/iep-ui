ee6493ad14bf2ca75f4383f490370ea764c96ab2
の半梦 <835806940@qq.com>
2019-03-19 13:47:11 +0800

    feat(y): y
    y

--- src/views/wel/message/SystemMessage/Page/Detail.vue
+++ src/views/wel/message/SystemMessage/Page/Detail.vue
@@ -145,6 +145,9 @@ export default {
         .text-bottom {
           display: flex;
           justify-content: space-between;
+          p {
+            margin-bottom: 0;
+          }
         }
       }
     }

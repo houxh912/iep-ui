be22828d32d8ec388f330661f5d130e699a3f916
严麒麟 <1121051063@qq.com>
2019-03-27 19:40:19 +0800

    feat(详情相似客户修改): 详情相似客户修改
    详情相似客户修改

--- src/views/crms/Customer/Page/CustomerPanorama/index.vue
+++ src/views/crms/Customer/Page/CustomerPanorama/index.vue
@@ -66,7 +66,7 @@
         <div class="item">
           <div class="label">相似客户：</div>
           <div class="value">
-            <div class="dot" v-for="(item,index) in formData.similarClient" :key="index">{{item}}</div>
+            <div class="dot" v-for="(item,index) in formData.similarClient" :key="index">{{item.commonName}}</div>
           </div>
         </div>
         <div class="item">

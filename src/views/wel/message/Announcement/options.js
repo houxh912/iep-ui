21014019bff3dd0d0977a04db4d6645a828c85ec
张超 <zhangchao@126.com>
2019-03-30 16:51:35 +0800

    fix(通知): 修复时间未显示问题

--- src/views/wel/message/Announcement/options.js
+++ src/views/wel/message/Announcement/options.js
@@ -18,11 +18,14 @@ const columnsMap = [
 
 const initForm = () => {
 	return {
-		id: '',
-		name: '',
-		content: '',
-		prop: '',
-		area: '',
+		id: null,  // 通知公告id
+		name: null,  // 通知公告Title
+		time: null, // 通知公告时间
+		senderName: null, // 发布人
+		receiverName: null, // 接收人
+		content: null, // 发布人
+		typeId: null, // 邀请联盟,会议纪要
+		isMark: null, // 是否标记
 	}
 }
 const formToDto = (row) => {

bc62220e941f16d5c788dc3ef76359f837f0070f
张超 <zhangchao@126.com>
2019-04-09 14:35:48 +0800

    feat(新增): 正在开发页面

--- src/views/wel/dynamic/index.vue
+++ src/views/wel/dynamic/index.vue
@@ -1,3 +1,20 @@
 <template>
-  <div>dynamic/index</div>
+  <img :src="fackerImg" />
 </template>
+<script>
+const fackerImg = require('./facker.png')
+export default {
+  data () {
+    return {
+      fackerImg,
+    }
+  },
+}
+</script>
+<style scoped>
+img {
+  margin: 0 auto;
+  width: 500px;
+  display: block;
+}
+</style>
--- src/views/wel/tasks/index.vue
+++ src/views/wel/tasks/index.vue
@@ -1,3 +1,20 @@
 <template>
-  <div>我的任务</div>
+  <img :src="fackerImg" />
 </template>
+<script>
+const fackerImg = require('./facker.png')
+export default {
+  data () {
+    return {
+      fackerImg,
+    }
+  },
+}
+</script>
+<style scoped>
+img {
+  margin: 0 auto;
+  width: 500px;
+  display: block;
+}
+</style>

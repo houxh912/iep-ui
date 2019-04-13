609790b6750e0c6fe88060eb58da082be2116d3f
张超 <zhangchao@126.com>
2019-04-13 14:29:34 +0800

    fix(审批): 改动

--- src/views/wel/approval/Components/New/options.js
+++ src/views/wel/approval/Components/New/options.js
@@ -2,40 +2,39 @@ import { mergeByFirst } from '@/util/util'
 import { initNow } from '@/util/date'
 const initForm = () => {
 	return {
-		id: null, // ID
-		userId: null, // 申请人ID
-		avatar: null, // 申请人avatar
-		name: null,
-		positionName: null,
-		job: null,
-		title: null,
-		entryTime: null,
+		id: '', // ID
+		userId: '', // 申请人ID
+		avatar: '', // 申请人avatar
+		name: '',
+		positionName: '',
+		job: '',
+		title: '',
+		entryTime: '',
 		nowTime: initNow(),
-		positiveTime: null,
-		transferTime: null,
+		positiveTime: '',
+		transferTime: '',
 		deptList: [],
 		type: 1, // 审批类型（字典：1-请假申请；2-出差申请；3-加班申请；4-请假申请；5-离职申请；6-调岗申请；7-招聘申请）
 		startTime: initNow(), // 开始时间(1:请假开始时间；2:出差开始时间;3:加班开始时间;4:入职时间;5:入职时间）
 		endTime: initNow(), // 结束时间(1:请假结束时间；2:出差结束时间;3:加班结束时间;4:转正时间;5:离职时间;6:调岗时间）
-		reason: null, // 申请理由
+		reason: '', // 申请理由
 		attachFile: [], // 文件
 		annex: [], // 文件
 		approver: [], // 审批人
 		cc: [], // 抄送人
-		duration: null, //各种时长
-		leaveType: null, // 请假类型
-		transferDept: null, // 部门ID
-		transferPosition: null, // 岗位ID
-		transferPositionList: [], // 岗位ID
-		busTripDistrict: null,
+		duration: '', //各种时长
+		leaveType: '', // 请假类型
+		transferDept: '', // 部门ID
+		transferPosition: '', // 岗位ID
+		busTripDistrict: '',
 	}
 }
 
 const formToVo = (row) => {
 	const newForm = mergeByFirst(initForm(), row)
-	newForm.annex = row.attachFile ? [row.attachFile] : []
-	newForm.approver = row.approverList
-	newForm.cc = row.ccList
+	newForm.annex = row.attachFile
+	newForm.approver = row.approverList || []
+	newForm.cc = row.ccList || []
 	return newForm
 }
 
@@ -56,10 +55,13 @@ const initSelfForm = () => {
 
 const formToDto = (row, type) => {
 	const newForm = { ...row, type }
-	newForm.attachFileUrl = newForm.annex.map(m => m.url)[0]
-	newForm.approver = newForm.approver.map(m => m.id)
+	newForm.attachFileUrl = row.annex.map(m => m.url)[0]
+	newForm.transferDept = row.transferDept
+	newForm.transferPosition = row.transferPositionList[row.transferPositionList.length - 1]
+	newForm.approver = row.approver.map(m => m.id)
+	newForm.cc = row.cc.map(m => m.id)
 	delete newForm.annex
-	newForm.cc = newForm.cc.map(m => m.id)
+	delete newForm.transferPositionList
 	return newForm
 }
 

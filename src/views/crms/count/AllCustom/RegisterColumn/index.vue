9484ea12165f6db49f083a7f6bc38cf506ee4c20
tjl <tjlzxl@163.com>
2019-04-18 19:08:54 +0800

    feat(客户统计我的客户接口组件修改完毕): 客户统计我的客户接口组件修改完毕
    客户统计我的客户接口组件修改完毕

--- src/views/crms/count/AllCustom/RegisterColumn/index.vue
+++ src/views/crms/count/AllCustom/RegisterColumn/index.vue
@@ -9,8 +9,8 @@
     </div>
     <div>
       <el-row>
-        <el-col :span=12>
-          <div class="top">
+        <el-col :span='12'>
+          <div class="top flag">
             <span>部门统计</span>
             <div class="more">
               <span class="border" @click="left"><i class="el-icon-arrow-left"></i></span>
@@ -21,15 +21,15 @@
             <count></count>
           </div>
         </el-col>
-        <el-col :span=12>
-          <div class="top">市场经理</div>
+        <el-col :span='12'>
+          <div class="top flag">市场经理</div>
           <div class="echarts">
             <manager></manager>
           </div>
         </el-col>
       </el-row>
       <el-row>
-        <el-col :span=12>
+        <el-col :span='12'>
           <div class="top">
             <span>区域类型</span>
           </div>
@@ -37,8 +37,15 @@
             <area-type> </area-type>
           </div>
         </el-col>
-        <el-col :span=12>
-          <div class="top">客户增长统计</div>
+        <el-col :span='12'>
+          <div class="top">
+            <span>客户增长统计</span>
+            <div class="inline">
+              <el-radio-group v-model="type" size="mini" @change="changeType">
+                <el-radio-button v-for="tab in tabList" :label="tab.value" :key="tab.value">{{tab.label}}</el-radio-button>
+              </el-radio-group>
+            </div>
+          </div>
           <div class="echarts">
             <increase></increase>
           </div>
@@ -60,6 +67,8 @@ export default {
   data () {
     return {
       number: '',
+      type: '1',
+      tabList: [{ label: '按周', value: '1' }, { label: '按月', value: '2' }, { label: '季度', value: '3' }, { label: '年度', value: '4' }],
     }
   },
   created () {
@@ -68,6 +77,9 @@ export default {
     })
   },
   methods: {
+    changeType () {
+      this.$message.success('功能开发中')
+    },
     searchPage () {
       this.$message.success('功能开发中')
     },
@@ -94,7 +106,7 @@ export default {
   float: right;
 }
 .top {
-  padding: 10px;
+  padding: 40px 20px 15px 20px;
   .more {
     float: right;
     margin-right: 20px;
@@ -110,13 +122,17 @@ export default {
       }
     }
   }
+  .inline {
+    float: right;
+  }
 }
 .echarts {
   height: 300px;
-  padding: 5px 5px;
-  border: 1px solid #eee;
 }
 .odd {
   margin-right: 20px;
+}
+.flag {
+  padding-top: 10px;
 }
 </style>

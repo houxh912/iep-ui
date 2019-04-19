326ed02bb4824baf332d845ca07172355a89bfc7
张超 <zhangchao@126.com>
2019-04-19 11:57:20 +0800

    fix(审批): 批不通过的理由请控制输入长度。输入过长保存会报错

--- src/views/wel/approval/Components/New/Leaving/index.vue
+++ src/views/wel/approval/Components/New/Leaving/index.vue
@@ -48,8 +48,6 @@ export default {
   mixins: [mixins],
   data () {
     return {
-      startTime: '',
-      endTime: '',
     }
   },
   methods: {
--- src/views/wel/approval/Components/New/mixins.js
+++ src/views/wel/approval/Components/New/mixins.js
@@ -59,26 +59,27 @@ export default {
     },
     // 处理时间段
     dealTime (val1, val2) {
-      var str1 = val1.replace(/-/g, '/')
-      var time1 = Date.parse(new Date(str1))
-      var str2 = val2.replace(/-/g, '/')
-      var time2 = Date.parse(new Date(str2))
-      var duration = Math.floor((time2 - time1) / 60000)
-      var hours = Math.floor(duration / 60)
-      var minutes = duration - hours * 60
-      if (time2 < time1) {
+      function parseDate (date) {
+        return new Date(date)
+      }
+      const newDate1 = parseDate(val1)
+      const newDate2 = parseDate(val2)
+      const between = (Number(newDate2) - Number(newDate1)) / 1000
+      if (between < 0) {
         this.$message.error('开始时间不能大于结束时间！！！')
         return
       }
+      const hours = parseInt(between / 3600)
+      const minutes = parseInt((between - 3600 * hours) / 60)
       this.form.duration = hours + '小时' + minutes + '分钟'
     },
     startChange (val) {
-      this.startTime = val
-      this.dealTime(val, this.endTime)
+      this.form.startTime = val
+      this.dealTime(val, this.form.endTime)
     },
     endChange (val) {
-      this.endTime = val
-      this.dealTime(this.startTime, val)
+      this.form.endTime = val
+      this.dealTime(this.form.startTime, val)
     },
   },
 }

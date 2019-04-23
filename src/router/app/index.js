81816fd61d9f450e3a619bfe1241019254eb1766
gold <291817075@qq.com>
2019-04-23 10:52:58 +0800

    fix(资源库加到一级菜单): 资源库加到一级菜单

--- src/router/app/index.js
+++ src/router/app/index.js
@@ -60,6 +60,36 @@ export default [
         name: '搜索频道',
         component: () => import('@/views/app/search/'),
       },
+      {
+        path: 'material',
+        name: '材料库',
+        component: () => import('@/views/app/material/'),
+      },
+      {
+        path: 'training',
+        name: '培训库',
+        component: () => import('@/views/app/training/'),
+      },
+      {
+        path: 'client',
+        name: '客户库',
+        component: () => import('@/views/app/client/'),
+      },
+      {
+        path: 'qualification',
+        name: '资质库',
+        component: () => import('@/views/app/qualification/'),
+      },
+      {
+        path: 'business',
+        name: '商机库',
+        component: () => import('@/views/app/business/'),
+      },
+      {
+        path: 'expert',
+        name: '专家库',
+        component: () => import('@/views/app/expert/'),
+      },
     ],
   },
 ]
---
+++ src/views/app/expert/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      专家库
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+}
+</script>
+<style scoped>
+</style>

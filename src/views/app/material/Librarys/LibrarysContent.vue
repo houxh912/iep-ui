ecc96b22b3ad7728531edea13399ea9bb5007387
gold <291817075@qq.com>
2019-04-24 09:24:04 +0800

    fix(材料库): 材料库

---
+++ src/views/app/material/Librarys/LibrarysContent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="librarys-content">
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+}
+</script>
---
+++ src/views/app/material/Librarys/LibrarysHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="librarys-header">
+    <div class="left">
+      <div :label="title" name="first">{{title}}<span>{{data}}</span></div>
+    </div>
+    <div class="right">
+      <operation-search @search-page="searchPage" advance-search>
+        <advance-search @search-page="searchPage"></advance-search>
+      </operation-search>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      title: '材料库',
+    }
+  },
+}
+</script>
+<style lang="scss" scoped>
+.librarys-header{
+  height: 52px;
+  padding-top: 8px;
+  border-bottom: solid 1px #e6e6e6;
+  .left{
+    float: left;
+    > div{
+      font-size: 18px;
+      line-height: 42px;
+      padding:0 10px;
+      border-bottom: 2px solid #ba1b21;
+    }
+  }
+  .right{
+    float: right;
+  }
+}
+</style>

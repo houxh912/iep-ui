4dd8ddb5897ad3853f1daedff86b68d8eebdf0d4
gold <291817075@qq.com>
2019-04-28 09:31:32 +0800

    fix(新技术详情): 新技术详情

---
+++ src/views/app/technologyDetails/Introduction.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="introduction">
+    <div class="left">
+      <i class="iconfont" :class="icon"></i>
+    </div>
+    <div class="right">
+      <div class="title">
+        <span class="name">{{name}}</span>
+        <span class="status">{{status}}</span>
+      </div>
+      <div class="tag">
+        <span v-for="(item,index) in tag" :key="index" >{{item}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      icon:'icon-tongyongleiziyuanpeizhi',
+      name:'Ant Design Pro',
+      status: '已完成',
+      tag:['产品设计','项目管理','原型设计','平台规划','需求分析','流程设计','产品培训','需求文档'],
+      updateTime:'2019-04-26 15:30',
+    }
+  },
+}
+</script>
+<style lang="scss" scoped>
+.introduction {
+  overflow: hidden;
+  .left{
+    width: 80px;
+    height: 80px;
+    line-height: 80px;
+    border: 1px solid #ebeaea;
+    border-radius: 2px;
+    background-color: #f8f8f8;
+    text-align: center;
+    margin-right: 30px;
+    float: left;
+    > i{
+      font-size: 60px;
+      color: #999;
+    }
+  }
+  .right{
+    float: left;
+    width:770px;
+    .title{
+      height: 50px;
+      line-height: 40px;
+      .name{
+        font-size: 20px;
+        color: #333;
+      }
+      .status{
+        background: #f8f8f8;
+        border-radius: 12px;
+        padding: 2px 10px;
+        border: 1px solid #ebeaea;
+        margin-left: 10px;
+      }
+    }
+    .tag{
+      > span{
+        display: inline;
+        list-style: none;
+        border: 1px solid #ebeaea;
+        border-radius: 4px;
+        padding: 4px 8px;
+        margin: 5px;
+        color: #999;
+      }
+    }
+  }
+}
+</style>
+<style scoped>
+.introduction >>> .el-card__body{
+  padding: 0;
+}
+</style>
---
+++ src/views/app/technologyDetails/TechnicalApplication.vue
@@ -0,0 +1,68 @@
+
+<template>
+  <div class="technical-application">
+    <div class="title">
+      <div>{{title}}</div>
+    </div>
+    <div class="piece" v-for="(item,index) in dataList" :key="index">
+      <div>
+        <i class="iconfont" :class="item.icon"></i>
+      </div>
+      <span>{{item.name}}</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      title:'相关技术应用',
+      dataList: [
+        {icon:'icon-tongyongleiziyuanpeizhi',name:'资源配置模块'},
+        {icon:'icon-tongyongleiziyuanpeizhi',name:'资源配置模块'},
+        {icon:'icon-tongyongleiziyuanpeizhi',name:'资源配置模块'},
+        {icon:'icon-tongyongleiziyuanpeizhi',name:'资源配置模块'},
+      ],
+    }
+  },
+}
+</script>
+<style lang="scss" scoped>
+.title{
+  font-size: 16px;
+  height: 60px;
+  line-height: 60px;
+  color: #333;
+}
+.technical-application{
+  overflow: hidden;
+  border-top: 1px solid #f5f5f5;
+  margin-top:20px; 
+  .piece{
+    float: left;
+    width: 100px;
+    margin-right: 30px;
+    > div{
+      width: 50px;
+      height: 50px;
+      line-height: 50px;
+      text-align: center;
+      border: 1px solid #dcdcdc;
+      margin: 0 auto;
+      > i{
+        font-size: 40px;
+        color: #999;
+      }
+    }
+    span{
+      width: 1005;
+      height: 36px;
+      line-height: 36px;
+      text-align: center;
+      display: block;
+    }
+  }
+}
+</style>
+<style scoped>
+</style>

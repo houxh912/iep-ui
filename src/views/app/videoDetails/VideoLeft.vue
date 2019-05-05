bee78a40e186eb2f12052af48f515491168a6c29
の半梦 <835806940@qq.com>
2019-05-05 15:44:49 +0800

    feat(視頻詳情): 視頻詳情
    視頻詳情
    
    BREAKING CHANGE: 視頻詳情

---
+++ src/views/app/videoDetails/VideoLeft.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <video width="900" height="500" controls>
+      <source :src="video" controls poster="img/video/video.jpg" alt="">
+    </video>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      video: '',
+    }
+  },
+}
+</script>
--- src/views/app/videoDetails/index.vue
+++ src/views/app/videoDetails/index.vue
@@ -1,3 +1,31 @@
 <template>
-  <iep-to-dev></iep-to-dev>
+  <div class="video">
+    <div class="video-con">
+      <video-left></video-left>
+      <video-right></video-right>
+    </div>
+    <video-operate></video-operate>
+  </div>
 </template>
+<script>
+import VideoLeft from './VideoLeft'
+import VideoRight from './VideoRight/'
+import VideoOperate from './VideoOperate'
+export default {
+  components: { VideoLeft, VideoRight, VideoOperate },
+  data () {
+    return {}
+  },
+}
+</script>
+<style lang="scss" scoped>
+.video {
+  margin: 0 auto;
+  width: 1200px;
+}
+.video-con {
+  display: flex;
+  margin-top: 30px;
+  justify-content: space-between;
+}
+</style>

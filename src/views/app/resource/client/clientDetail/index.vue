bc063d51d7d27001144b16a34e4fffdd8611bd1c
zhnagqi <850629074@qq.com>
2019-05-07 15:00:09 +0800

    fix(客户详情细节修改): 客户详情细节修改
    客户详情细节修改

--- src/views/app/resource/client/clientDetail/index.vue
+++ src/views/app/resource/client/clientDetail/index.vue
@@ -1,24 +1,27 @@
 <template>
-  <div class="material-detail">
-    <div class="library">
-      <div class="client-top">
-        <p>{{title}}</p>
-        <span>市场经理：{{name[0]}}</span>
-        <span>协助人：{{name[1]}}</span>
+  <div>
+    <div class="client-detail">
+      <div class="library">
+        <div class="client-top">
+          <p>{{title}}</p>
+          <span>市场经理：{{name[0]}}</span>
+          <span>协助人：{{name[1]}}</span>
+        </div>
+        <customer></customer>
       </div>
-      <customer></customer>
+      <div class="piece">
+        <IepAppTabCard :title="labelTitle">
+          <IepAppLabelCard :dataList="labelList"></IepAppLabelCard>
+        </IepAppTabCard>
+        <IepAppTabCard :title="listTitle1">
+          <IepAppListCard :dataList="listList1"></IepAppListCard>
+        </IepAppTabCard>
+        <IepAppTabCard :title="rankingTitle">
+          <IepAppRankingCard :dataList="dataList"></IepAppRankingCard>
+        </IepAppTabCard>
+      </div>
     </div>
-    <div class="piece">
-      <IepAppTabCard :title="labelTitle">
-        <IepAppLabelCard :dataList="labelList"></IepAppLabelCard>
-      </IepAppTabCard>
-      <IepAppTabCard :title="listTitle1">
-        <IepAppListCard :dataList="listList1"></IepAppListCard>
-      </IepAppTabCard>
-      <IepAppTabCard :title="rankingTitle">
-        <IepAppRankingCard :dataList="dataList"></IepAppRankingCard>
-      </IepAppTabCard>
-    </div>
+    <IepAppFooterBar></IepAppFooterBar>
   </div>
 </template>
 <script>
@@ -61,7 +64,7 @@ export default {
     font-size: 14px;
   }
 }
-.material-detail {
+.client-detail {
   width: 1200px;
   margin: 20px auto;
   border-top: 1px solid #eee;

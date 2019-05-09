229d4c2a3f8de9e64460337d4186e13d1092c783
gold <291817075@qq.com>
2019-05-09 11:47:58 +0800

    fix(建议列表): 建议列表

--- src/views/hrms/Suggestion/List.vue
+++ src/views/hrms/Suggestion/List.vue
@@ -1,3 +1,70 @@
 <template>
-  <iep-to-dev></iep-to-dev>
+  <div>
+    <basic-container>
+      <page-header title="我发出的建议"></page-header>
+      <operation-container>
+        <template slot="left">
+          <iep-button @click="handleAdd" type="primary" icon="el-icon-plus" plain>新增</iep-button>
+        </template>
+        <template slot="right">
+          <operation-search @search-page="searchPage" advance-search>
+            <advance-search @search-page="searchPage"></advance-search>
+          </operation-search>
+        </template>
+      </operation-container>
+      <iep-table :isLoadTable="false" :pagination="pagination" :dictsMap="dictsMap" :columnsMap="columnsMap" :pagedTable="pagedTable" @size-change="handleSizeChange" @current-change="handleCurrentChange" is-mutiple-selection>
+        <template slot="before-columns">
+        </template>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <operation-wrapper>
+              <iep-button type="warning" @click="handleReview(scope.row)" plain>查看</iep-button>
+              <iep-button :disabled="scope.row.status===3" @click="handleDeliver(scope.row)">转交</iep-button>
+            </operation-wrapper>
+          </template>
+        </el-table-column>
+      </iep-table>
+    </basic-container>
+  </div>
 </template>
+
+<script>
+import mixins from '@/mixins/mixins'
+import AdvanceSearch from './AdvanceSearch'
+import { dictsMap, columnsMap } from './options'
+export default {
+  components: {
+    AdvanceSearch,
+  },
+  mixins: [mixins],
+  data () {
+    return {
+      dictsMap,
+      columnsMap,
+      pagedTable:[
+        {name:'aaaa',theme:'内网2.0开发进度安排建议',status:'1',sendTime:'2019-05-09'},
+        {name:'aaaa',theme:'内网2.0开发进度安排建议',status:'1',sendTime:'2019-05-09'},
+      ],
+    }
+  },
+  created () {
+    this.loadPage()
+  },
+  methods: {
+    loadPage () {
+
+    },
+    handleCommandType () {
+      // console.log(val)
+    },
+    handleCommandUser () {
+      // console.log(val)
+    },
+    handleAdd () {
+
+    },
+  },
+}
+</script>
+<style scoped>
+</style>
---
+++ src/views/hrms/Suggestion/options.js
@@ -0,0 +1,42 @@
+// org config options
+const dictsMap = {
+  status: {
+    0: '草稿',
+    1: '未处理',
+    2: '以采纳',
+    3: '已驳回',
+  },
+}
+
+const columnsMap = [
+  {
+    prop: 'name',
+    label: '收件人',
+  },
+  {
+    prop: 'theme',
+    label: '主题',
+  },
+  {
+    prop: 'status',
+    label: '状态',
+    type: 'dict',
+  },
+  {
+    prop: 'sendTime',
+    label: '发送时间',
+  },
+]
+
+const initForm = () => {
+  return {
+    'id': '', // ID
+    'name': '', // 收件人
+    'theme': '', // 主题
+    'status': 1, // 状态
+    'sendTime': '',//发送时间
+  }
+}
+
+
+export { dictsMap, columnsMap, initForm }

535a70f72ef7e5487d6a43f8cacb40e1237a11e9
张超 <zhangchao@126.com>
2019-05-13 11:20:49 +0800

    feat(多色): 图标

--- src/config/env.js
+++ src/config/env.js
@@ -5,7 +5,7 @@ const env = process.env
 const baseUrl = ''
 
 const iconfontVersion = ['1036949_1hxxskrvymg']
-const multicolorIconfontUrl = '//at.alicdn.com/t/font_1184303_aqpz3ebm4pk.js'
+const multicolorIconfontUrl = '//at.alicdn.com/t/font_1184303_hanb888rdlc.js'
 const iconfontUrl = '//at.alicdn.com/t/font_$key.css'
 
 const codeUrl = '/api/code'

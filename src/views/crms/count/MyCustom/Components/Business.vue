ce2dfc1b7026aadf6d416746f4c90ef9b6cf5d5f
严麒麟 <1121051063@qq.com>
2019-05-15 16:15:43 +0800

    feat(客户统计|客户分析): 业务类型对接
    业务类型对接

--- src/views/crms/count/MyCustom/Components/Business.vue
+++ src/views/crms/count/MyCustom/Components/Business.vue
@@ -14,12 +14,12 @@ export default {
       color: ['#A9CCF0', '#8D99B3', '#F0F0A9', '#FFD9BC', '#F1C8C8'],
       tooltip: {
         trigger: 'item',
-        formatter: '{b}：{c}%',
+        formatter: '{b}：{d}%',
       },
       label: {
         show: true,
         color: '#000',
-        formatter: '{b}：{c}%',
+        formatter: '{b}：{d}%',
       },
       series: {
         center: ['50%', '50%'],
@@ -30,9 +30,12 @@ export default {
       data: [
         { value: '0', name: '咨询', label: 'consulting' },
         { value: '0', name: '数据', label: 'information' },
-        { value: '0', name: '软件', label: 'softwareNumber' },
+        { value: '0', name: '会议培训', label: 'meetingTraining' },
+        { value: '0', name: '业务类型其他', label: 'othersBusiness' },
+        { value: '0', name: '外包', label: 'outsourcing' },
         { value: '0', name: '平台', label: 'platform' },
-        { value: '0', name: '事项', label: 'matters' },
+        { value: '0', name: '产品', label: 'product' },
+        { value: '0', name: '技术服务', label: 'technicalService' },
       ],
       chartData: {
         columns: ['name', 'value'],
@@ -46,13 +49,16 @@ export default {
   methods: {
     load () {
       getMyBusiness().then((res) => {
+        console.log(res)
         if (res) {
           this.loading = false
         }
+        console.log(this.data)
         Object.keys(res.data.data).forEach((item) => {
           var index = _.findIndex(this.data, function (o) { return o.label == item })
           this.data[index].value = res.data.data[item]
         })
+
         this.chartData.rows = this.data
       })
     },

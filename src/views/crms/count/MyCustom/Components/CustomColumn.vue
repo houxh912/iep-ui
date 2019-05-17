0a52a7bfcc45881376c18636883422b240d8a3f3
严麒麟 <1121051063@qq.com>
2019-05-17 16:36:37 +0800

    feat(客户统计|我的客户): 我的客户客户分析客户建议对接
    我的客户客户分析客户建议对接

--- src/views/crms/count/MyCustom/Components/CustomColumn.vue
+++ src/views/crms/count/MyCustom/Components/CustomColumn.vue
@@ -18,10 +18,10 @@
         <div class="echarts lines">
           <div class="line"></div>
           <div class="msg">您的客户中：</div>
-          <div class="msg">园区级，平台类客户居多</div>
+          <div class="msg">{{region}}，{{proposal}}类客户居多</div>
           <div class="msg">平均拜访次数：<span class="color">3</span></div>
           <div class="msg">平均方案上传：<span class="color">4</span></div>
-          <div class="suggest">建议多寻找省级客户。</div>
+          <div class="suggest">建议多寻找{{info}}类客户。</div>
         </div>
       </el-col>
     </el-row>
@@ -30,12 +30,93 @@
 <script>
 import District from './District'
 import Business from './Business'
+import { getDistrictMin, getDistrictMax, getMyBusinessMax } from '@/api/crms/count'
 export default {
   components: { District, Business },
   data () {
     return {
-
+      data: [
+        { value: '0', name: '部委', label: 'ministriesCommissions' },
+        { value: '0', name: '省级', label: 'provincialLevel' },
+        { value: '0', name: '市级', label: 'cities' },
+        { value: '0', name: '区级', label: 'county' },
+        { value: '0', name: '园区', label: 'park' },
+        { value: '0', name: '企业', label: 'enterprise' },
+        { value: '0', name: '其他', label: 'other' },
+      ],
+      data1: [
+        { value: '0', name: '咨询', label: 'consulting' },
+        { value: '0', name: '数据', label: 'information' },
+        { value: '0', name: '会议培训', label: 'meetingTraining' },
+        { value: '0', name: '业务类型其他', label: 'othersBusiness' },
+        { value: '0', name: '外包', label: 'outsourcing' },
+        { value: '0', name: '平台', label: 'platform' },
+        { value: '0', name: '产品', label: 'product' },
+        { value: '0', name: '技术服务', label: 'technicalService' },
+      ],
+      info: '',
+      business: [],
+      region: '',
+      type: '',
+      businessMax: [],
+      aaa: [],
+      proposal: '',
     }
+  },
+  created () {
+    this.load()
+  },
+  methods: {
+    load () {
+      getDistrictMin().then(res => {
+        let keys = []
+        for (let index = 0; index < res.data.length; index++) {
+          for (let key in res.data[index]) {
+            keys.push(key)
+          }
+        }
+        for (let i = 0; i < this.data.length; i++) {
+          for (let index = 0; index < keys.length; index++) {
+            if (this.data[i].label == keys[index]) {
+              this.business.push(this.data[i].name)
+            }
+          }
+        }
+        this.info = this.business.join('，')
+      })
+      getDistrictMax().then(res => {
+        let keys = []
+        for (let index = 0; index < res.data.length; index++) {
+          for (let key in res.data[index]) {
+            keys.push(key)
+          }
+        }
+        for (let i = 0; i < this.data.length; i++) {
+          for (let index = 0; index < keys.length; index++) {
+            if (this.data[i].label == keys[index]) {
+              this.businessMax.push(this.data[i].name)
+            }
+          }
+        }
+        this.region = this.businessMax.join('，')
+      })
+      getMyBusinessMax().then(res => {
+        let keys = []
+        for (let index = 0; index < res.data.length; index++) {
+          for (let key in res.data[index]) {
+            keys.push(key)
+          }
+        }
+        for (let i = 0; i < this.data1.length; i++) {
+          for (let index = 0; index < keys.length; index++) {
+            if (this.data1[i].label == keys[index]) {
+              this.aaa.push(this.data1[i].name)
+            }
+          }
+        }
+        this.proposal = this.aaa.join('，')
+      })
+    },
   },
 }
 </script>

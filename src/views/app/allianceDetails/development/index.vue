03fe33b35af217c90be521758ad6ae9defddc069
zhnagqi <850629074@qq.com>
2019-05-17 10:53:13 +0800

    fix(fix): fix
    fix

--- src/views/app/allianceDetails/development/index.vue
+++ src/views/app/allianceDetails/development/index.vue
@@ -1,3 +1,38 @@
 <template>
-    <iep-to-dev></iep-to-dev>
+  <div class="events">
+    <el-timeline>
+      <el-timeline-item v-for="(activity, index) in activities" :key="index" :icon="activity.icon" :type="activity.type" :color="activity.color" :size="activity.size" :timestamp="activity.timestamp">
+        {{activity.content}}
+      </el-timeline-item>
+    </el-timeline>
+  </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      activities: [{
+        content: '支持使用图标',
+        timestamp: '2018-04-12 20:46',
+        size: 'large',
+        icon: 'el-icon-time',
+      }, {
+        content: '支持自定义颜色',
+        timestamp: '2018-04-03 20:46',
+      }, {
+        content: '支持自定义尺寸',
+        timestamp: '2018-04-03 20:46',
+        size: 'large',
+      }, {
+        content: '默认样式的节点',
+        timestamp: '2018-04-03 20:46',
+      }],
+    }
+  },
+}
+</script>
+<style lang="scss" scoped>
+.events {
+  margin: 40px;
+}
+</style>

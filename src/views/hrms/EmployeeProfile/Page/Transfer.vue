3c0c49f18d7f3d3495df596cf6b29d07c3900933
zhangchao <zhangchao@126.com>
2019-05-17 11:22:39 +0800

    fix(人力): 编辑去掉编辑

--- src/views/hrms/EmployeeProfile/Page/Transfer.vue
+++ src/views/hrms/EmployeeProfile/Page/Transfer.vue
@@ -1,11 +1,13 @@
 <template>
-  <inline-form-table :table-data="value.transfer" :columns="transferColumns" requestName="transfer" type="employee_profile" :rid="value.id" @load-page="handleSave"></inline-form-table>
+  <!-- <inline-form-table :table-data="value.transfer" :columns="transferColumns" requestName="transfer" type="employee_profile" :rid="value.id" @load-page="handleSave"></inline-form-table> -->
+  <inline-form-table-detail :data="value.transfer" :columns="transferColumns"></inline-form-table-detail>
 </template>
 <script>
-import InlineFormTable from '@/views/hrms/Components/InlineFormTable/'
-import { transferColumns} from '@/views/hrms/Components/options'
+// import InlineFormTable from '@/views/hrms/Components/InlineFormTable/'
+import InlineFormTableDetail from '@/views/hrms/Components/InlineFormTable/detail'
+import { transferColumns } from '@/views/hrms/Components/options'
 export default {
-  components: {InlineFormTable},
+  components: { InlineFormTableDetail },
   props: ['value'],
   data () {
     return {

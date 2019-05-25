a09333cb09160c360ae0884ac7006d6e1da96db0
张超 <zhangchao@126.com>
2019-05-25 11:07:12 +0800

    feat(财务): 完成现金与银行日记账

--- src/views/fams/FinancialManagement/BankDepositJournal/index.vue
+++ src/views/fams/FinancialManagement/BankDepositJournal/index.vue
@@ -1,81 +1,90 @@
 <template>
   <div>
     <basic-container>
-      <page-header title="银行存款日记账" :replaceText="replaceText" :data="['国脉海洋']"></page-header>
+      <page-header title="银行存款日记账" :replaceText="replaceText" :data="statistics"></page-header>
       <operation-container>
         <template slot="left">
-          <div class="table-heading">舟山国脉海洋舟山建行支行（工资支出）2019年四月</div>
+          <iep-select v-model="companyId" autocomplete="off" prefix-url="fams/company" placeholder="请选择公司" @change="searchPage()"></iep-select>
+          <iep-select v-if="!bankOption.disabled" v-model="bankId" autocomplete="off" :prefix-url="bankOption.prefixUrl" placeholder="请选择银行账户" @change="searchPage()"></iep-select>
         </template>
         <template slot="right">
-          <operation-search @search="searchPage" advance-search>
-            <el-form :model="paramForm" label-width="100px" size="mini">
-              <el-form-item label="关键字：">
-                <el-input v-model="paramForm.applicant"></el-input>
-              </el-form-item>
-              <el-form-item label="所属公司：">
-                <el-select v-model="paramForm.company" placeholder="请选择所属公司">
-                  <el-option v-for="item in company" :key="item.value" :label="item.label" :value="item.value">
-                  </el-option>
-                </el-select>
-              </el-form-item>
-              <el-form-item label="现金账户：">
-                <el-select v-model="paramForm.cashAccount" placeholder="请选择现金账户">
-                  <el-option v-for="item in cashAccount" :key="item.value" :label="item.label" :value="item.value">
-                  </el-option>
-                </el-select>
-              </el-form-item>
-              <el-form-item label="时间：">
-                <el-date-picker v-model="paramForm.date" type="date" placeholder="选择日期">
-                </el-date-picker>
-              </el-form-item>
-              <el-form-item>
-                <el-button type="primary" @click="searchPage">搜索</el-button>
-                <el-button>取消</el-button>
-              </el-form-item>
-            </el-form>
-          </operation-search>
+          <iep-date-picker v-model="yearMonth" align="right" type="month" placeholder="选择年月" @change="searchPage()"></iep-date-picker>
         </template>
       </operation-container>
-      <iep-table :isLoadTable="isLoadTable" :pagination="pagination" :dictsMap="dictsMap" :columnsMap="columnsMap" :pagedTable="pagedTable" @size-change="handleSizeChange" @current-change="handleCurrentChange" show-summary :summary-method="getSummaries">
+      <iep-table :isLoadTable="isLoadTable" :is-pagination="false" :columnsMap="columnsMap" :pagedTable="pagedTable" show-summary :summary-method="getSummaries">
         <el-table-column label="金额">
-          <el-table-column prop="income" label="收入">
+          <el-table-column prop="inCome" label="收入">
           </el-table-column>
           <el-table-column prop="expenditure" label="支出">
           </el-table-column>
-          <el-table-column prop="balance" label="本期余额">
+          <el-table-column prop="currentBalance" label="本期余额">
           </el-table-column>
         </el-table-column>
-        <el-table-column prop="remarks" label="备注">
+        <el-table-column prop="remark" label="备注">
         </el-table-column>
       </iep-table>
     </basic-container>
-    <iep-review-confirm ref="iepReviewForm" @load-page="loadPage"></iep-review-confirm>
   </div>
 </template>
 
 <script>
-import { getFinancialManagementPage } from '@/api/fams/financial_management'
-import mixins from '@/mixins/mixins'
-import { columnsMap, dictsMap, initSearchForm } from './options'
+import { getBankDiaryList } from '@/api/fams/statistics'
+import { columnsMap, initSearchForm, initNow, getYear, getMonth } from './options'
 export default {
-  mixins: [mixins],
   data () {
     return {
-      dictsMap,
       columnsMap,
-      company: '',
-      cashAccount: '',
-      paramForm: initSearchForm(),
-      replaceText: (data) => `（所属公司：${data[0]}）`,
-
+      yearMonth: initNow(),
+      companyId: '',
+      bankId: '',
+      isLoadTable: true,
+      pagedTable: [],
+      searchForm: initSearchForm(),
+      statistics: [0, 0],
+      replaceText: (data) => `（累计总支出：${data[0]}元，累计总收入：${data[1]}元）`,
     }
+  },
+  computed: {
+    bankOption () {
+      if (this.companyId) {
+        return {
+          disabled: false,
+          prefixUrl: `fams/bank_account/${this.companyId}`,
+        }
+      } else {
+        return {
+          disabled: true,
+          prefixUrl: `fams/bank_account/${this.companyId}`,
+        }
+      }
+    },
+    newSearchForm () {
+      return {
+        companyId: this.companyId,
+        bankId: this.bankId,
+        year: getYear(this.yearMonth),
+        month: getMonth(this.yearMonth) + 1,
+      }
+    },
   },
   created () {
     this.loadPage()
   },
   methods: {
+    async loadTable (param, requestFn) {
+      this.isLoadTable = true
+      const { data } = await requestFn({ ...param, ...this.pageOption })
+      this.companyId = data.data.companyId
+      this.bankId = data.data.bankId
+      this.pagedTable = data.data.diaryList
+      this.statistics = [data.data.expenditureTotal, data.data.inComeTotal]
+      this.isLoadTable = false
+    },
+    searchPage () {
+      this.loadPage(this.newSearchForm)
+    },
     loadPage (param = this.searchForm) {
-      this.loadTable(param, getFinancialManagementPage)
+      this.loadTable(param, getBankDiaryList)
     },
     getSummaries (param) {
       const { columns, data } = param
@@ -97,7 +106,7 @@ export default {
           }, 0)
           sums[index] += ' 元'
         } else {
-          sums[index] = 'N/A'
+          sums[index] = ''
         }
       })
 
--- src/views/fams/FinancialManagement/BankDepositJournal/options.js
+++ src/views/fams/FinancialManagement/BankDepositJournal/options.js
@@ -1,78 +1,27 @@
-import { mergeByFirst } from '@/util/util'
-// org config options
-const dictsMap = {
-  status: {
-    0: '审核通过',
-    1: '待审核',
-    2: '审核驳回',
-  },
-}
+import { initNow, getYear, getMonth } from '@/util/date'
 
 const columnsMap = [
   {
-    prop: 'number',
+    prop: 'id',
     label: '序号',
   },
   {
-    prop: 'date',
+    prop: 'createTime',
     label: '日期',
   },
   {
-    prop: 'abstract',
+    prop: 'remark',
     label: '摘要',
   },
-  // {
-  //   prop: 'money',
-  //   label: '金额',
-  //   children:[
-  //     {
-  //       prop: 'aaaa',
-  //       label: '收入',
-  //     },
-  //     {
-  //       prop: 'bbbb',
-  //       label: '支出',
-  //     },
-  //     {
-  //       prop: 'cccc',
-  //       label: '本期余额',
-  //     },
-  //   ],
-  // },
-  // {
-  //   prop: 'departments',
-  //   label: '备注',
-  // },
 ]
-
-const initForm = () => {
-  return {
-    isOpen: false,
-    intro: '',
-  }
-}
-
-const initDtoForm = () => {
-  return {
-    isOpen: false,
-    intro: '',
-  }
-}
-
-const formToDto = (row) => {
-  const newForm = mergeByFirst(initDtoForm(), row)
-  newForm.positionId = row.position[row.position.length - 1]
-  newForm.deptId = row.dept[row.dept.length - 1]
-  return newForm
-}
 
 const initSearchForm = () => {
   return {
-    keyword: '',
-    company: '',
-    cashAccount: '',
-    date: '',
+    year: new Date().getFullYear(),
+    companyId: '0',
+    bankId: '0',
+    month: new Date().getMonth()+1,
   }
 }
 
-export { dictsMap, columnsMap, initForm, formToDto, initSearchForm }
+export { columnsMap, initSearchForm, initNow, getYear, getMonth }

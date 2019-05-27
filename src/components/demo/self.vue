beb273f5aa4a2f7c49f89f19bd06cbda5b468b78
张超 <zhangchao@126.com>
2019-05-27 10:09:11 +0800

    feat(新增): 演示页面

---
+++ src/components/demo/self.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="iep-page-form">
+    <basic-container>
+      <el-form ref="form" :model="form" size="small" label-width="120px">
+        <el-checkbox v-model="checked">备选项</el-checkbox>
+        <el-radio-group v-model="radio">
+          <el-radio :label="3">备选项</el-radio>
+          <el-radio :label="6">备选项</el-radio>
+          <el-radio :label="9">备选项</el-radio>
+        </el-radio-group>
+      </el-form>
+    </basic-container>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      checked: '',
+      radio: '',
+    }
+  },
+}
+</script>

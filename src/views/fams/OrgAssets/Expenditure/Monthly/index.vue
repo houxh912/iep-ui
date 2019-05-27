e67964d8f23a9b0a84e1995d16e98dea98b7e8ea
张超 <zhangchao@126.com>
2019-05-27 10:26:47 +0800

    fix(财务): 完成组织支出

--- src/views/fams/OrgAssets/Expenditure/Monthly/index.vue
+++ src/views/fams/OrgAssets/Expenditure/Monthly/index.vue
@@ -3,11 +3,11 @@
     <operation-container>
       <template slot="left">
         <operation-wrapper>
-          <span>年度支出</span>
+          <span>月度支出</span>
         </operation-wrapper>
       </template>
       <template slot="right">
-        <iep-date-picker size="small" v-model="yearMonth" align="right" type="year" placeholder="选择年" @change="searchPage()"></iep-date-picker>
+        <iep-date-picker size="small" v-model="yearMonth" align="right" type="month" placeholder="选择年" @change="searchPage()"></iep-date-picker>
       </template>
     </operation-container>
     <iep-table :isLoadTable="isLoadTable" :height="tableHeight" :is-pagination="false" :columnsMap="columnsMap" :pagedTable="pagedTable" show-summary :summary-method="getSummaries" is-tree>
@@ -23,7 +23,7 @@
 <script>
 import { getSummaries } from '@/util/table'
 import { getExpenditureList } from '@/api/fams/statistics'
-import { columnsMap, initNow, getYear } from './options'
+import { columnsMap, initNow, getMonth } from './options'
 export default {
   data () {
     return {
@@ -37,7 +37,7 @@ export default {
   computed: {
     newSearchForm () {
       return {
-        year: getYear(this.yearMonth),
+        month: getMonth(this.yearMonth) + 1,
       }
     },
   },

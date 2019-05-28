37c666f378807532a01668a08364a5278c3279f0
huzhengw115 <huzhengw115@qq.com>
2019-05-28 09:31:23 +0800

    feat(频道页): 完成页面去除提示

--- src/views/app/person/index.vue
+++ src/views/app/person/index.vue
@@ -16,13 +16,6 @@ export default {
     return {
     }
   },
-  created () {
-    this.$notify({
-      title: '注意',
-      message: '此页面正在开发中，当前数据仅供参考。',
-      offset: 60,
-    })
-  },
 }
 </script>
 <style scoped>

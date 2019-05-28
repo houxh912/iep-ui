030182aecd24c84da11ecf7fa2573602ad8aada0
张超 <zhangchao@126.com>
2019-05-28 14:24:22 +0800

    feat(财务与组件): 更新功能与选项

---
+++ src/views/fams/GroupFinance/GroupFinancialFlow/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <basic-container>
+      <page-header title="集团财务流水"></page-header>
+      <operation-container>
+        <template slot="left">
+          <iep-button @click="handleAdd" type="primary" icon="el-icon-plus" plain>发行</iep-button>
+        </template>
+        <template slot="right">
+          <operation-search @search-page="searchPage"></operation-search>
+        </template>
+      </operation-container>
+      <iep-table :isLoadTable="isLoadTable" :pagination="pagination" :columnsMap="columnsMap" :pagedTable="pagedTable" @size-change="handleSizeChange" @current-change="handleCurrentChange">
+      </iep-table>
+    </basic-container>
+    <dialog-form ref="DialogForm" @load-page="loadPage"></dialog-form>
+  </div>
+</template>
+<script>
+import { getUnionBorrowPage } from '@/api/fams/org_borrow'
+import DialogForm from './DialogForm'
+import mixins from '@/mixins/mixins'
+import { columnsMap } from './options'
+export default {
+  mixins: [mixins],
+  components: { DialogForm },
+  data () {
+    return {
+      columnsMap,
+    }
+  },
+  created () {
+    // this.loadPage()
+    this.isLoadTable = false
+  },
+  methods: {
+    handleAdd () {
+      this.$refs['DialogForm'].dialogShow = true
+    },
+    loadPage (param = this.searchForm) {
+      this.loadTable(param, getUnionBorrowPage)
+    },
+  },
+}
+</script>
---
+++ src/views/fams/GroupFinance/GroupFinancialFlow/options.js
@@ -0,0 +1,20 @@
+const columnsMap = [
+	{
+		prop: 'inOrgName',
+		label: '发行数量',
+	},
+	{
+		prop: 'amount',
+		label: '当前用户余额',
+	},
+	{
+		prop: 'borrowMoneyType',
+		label: '操作人',
+	},
+	{
+		prop: 'repaymentTime',
+		label: '发行日期',
+	},
+]
+
+export { columnsMap }

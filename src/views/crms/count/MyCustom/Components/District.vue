6ba860a533df9dc7540566725b700358377b3453
严麒麟 <1121051063@qq.com>
2019-06-03 11:42:35 +0800

    feat(客户统计): 客户分析客户类型百分比修复
    客户分析客户类型百分比修复

--- src/views/crms/count/MyCustom/Components/District.vue
+++ src/views/crms/count/MyCustom/Components/District.vue
@@ -14,12 +14,12 @@ export default {
       color: ['#A9F0DF', '#D56368', '#F1C8C8', '#FFB880', '#F0F0A9', '#8D99B3', '#A9CCF0'],
       tooltip: {
         trigger: 'item',
-        formatter: '{b}：{c}%',
+        formatter: '{b}：{c}个 ({d}%)',
       },
       label: {
         show: true,
         color: '#000',
-        formatter: '{b}：{c}%',
+        formatter: '{b}：{d}%',
       },
       series: {
         center: ['50%', '50%'],

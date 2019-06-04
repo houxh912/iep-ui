f078cc2663cf8fe1719a40a6305d39f23fcdf53c
严麒麟 <1121051063@qq.com>
2019-06-04 14:36:33 +0800

    feat(联系人): 联系人导入导出功能添加

--- src/api/crms/excell.js
+++ src/api/crms/excell.js
@@ -1,8 +1,29 @@
 import request from '@/router/axios'
-
+// 客户模板下载
 export function downloadModel () {
   return request({
     url: '/crm/crms/iepclientinfoexcel/download',
+    method: 'post',
+    responseType: 'arraybuffer',
+  }).then(response => {
+    // 处理返回的文件流
+    let filename = response.headers['content-disposition'].split(';')[1]
+    filename = decodeURIComponent(filename.split('=')[1])
+    // filename = decodeURIComponent(filename.split('"')[1])
+    const blob = new Blob([response.data])
+    const link = document.createElement('a')
+    link.href = window.URL.createObjectURL(blob)
+    link.download = filename
+    document.body.appendChild(link)
+    link.style.display = 'none'
+    console.log(link)
+    link.click()
+  })
+}
+// 联系人模板下载
+export function downloadContactModel () {
+  return request({
+    url: '/crm/clientcontactexcel/export',
     method: 'post',
     responseType: 'arraybuffer',
   }).then(response => {
---
+++ src/views/crms/contact/Page/ExcellImport/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <iep-dialog :dialog-show="dialogShow" :title="`导入联系人模板`" width="520px" @close="close">
+    <el-upload class="upload-demo" drag :headers="token" :action="actionUrl" accept=".xlsx, .xls" :limit="1" :multiple="false" :on-success="handleFileFinish">
+      <i class="el-icon-upload"></i>
+      <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+      <div class="el-upload__tip" slot="tip"> 只能上传xls/xlsx文件
+        <el-button type="text" @click="handleDownloadModel()">下载模板</el-button>
+      </div>
+    </el-upload>
+  </iep-dialog>
+</template>
+<script>
+import { mapState } from 'vuex'
+import { downloadContactModel } from '@/api/crms/excell'
+
+export default {
+  props: {
+    urlName: {
+      type: String,
+      default: '',
+    },
+  },
+  data () {
+    return {
+      dialogShow: false,
+      token: {},
+      actionUrl: '',
+    }
+  },
+  computed: {
+    ...mapState({
+      access_token: state => state.user.access_token,
+    }),
+  },
+  created () {
+    this.actionUrl = this.urlName
+    this.token.Authorization = 'Bearer ' + this.access_token
+  },
+  methods: {
+    close () {
+      this.dialogShow = false
+    },
+    handleDownloadModel () {
+      downloadContactModel()
+    },
+    handleFileFinish (res) {
+      setTimeout(() => {
+        this.$emit('close', res)
+      }, 500)
+    },
+  },
+}
+</script>
+<style lang="scss" scoped>
+.upload-demo {
+  text-align: center;
+  padding-bottom: 10px;
+}
+</style>

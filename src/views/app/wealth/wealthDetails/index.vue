28b2b167da46ddb279b0f3bb23fc263c0878f676
gold <291817075@qq.com>
2019-06-05 14:38:07 +0800

    fix(投资修改): 投资修改

--- src/views/app/wealth/wealthDetails/index.vue
+++ src/views/app/wealth/wealthDetails/index.vue
@@ -22,6 +22,9 @@
                 <div>目标金额</div>
                 <div>{{form.targetAmount}}</div>
                 <el-button @click="handleAdd()" type="danger" size="medium" plain v-show="form.status==4">马上参与</el-button>
+                <el-button type="warning" size="medium" plain v-show="form.status==1">待审核</el-button>
+                <el-button type="info" size="medium" plain v-show="form.status==6">已结束</el-button>
+                <el-button type="info" size="medium" plain v-show="form.status==5 || form.status==2">待上架</el-button>
               </div>
             </div>
           </el-card>
@@ -206,7 +209,6 @@ export default {
       this.$refs['DialogForm'].form.investmentId = this.id
       this.$refs['DialogForm'].form.orgId = this.form.orgId
       this.$refs['DialogForm'].form.allSharesNumber = this.form.allSharesNumber
-      this.$refs['DialogForm'].form.targetAmount = this.form.targetAmount
       this.$refs['DialogForm'].form.sharesUnivalent = this.form.sharesUnivalent
       this.$refs['DialogForm'].dialogShow = true
     },

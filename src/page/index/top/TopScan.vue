e018cec64a30659a34e1427ae16bfa98e356e6ec
张超 <zhangchao@126.com>
2019-06-06 18:47:44 +0800

    fix(手机版): 二维码

---
+++ src/page/index/top/TopScan.vue
@@ -0,0 +1,45 @@
+<template>
+  <el-popover placement="bottom" width="336" v-model="visible" trigger="click">
+    <!-- TODO: -->
+    <iep-img src="//cloud.govmade.com/upload/iep/201906/491345f6-ac0b-449d-a78d-3e55bf669ae4_TIM%E5%9B%BE%E7%89%8720190606183914.jpg"></iep-img>
+    <el-badge :hidden="!emailNum" :value="emailNum || 0" slot="reference" class="item">
+      <el-tooltip effect="dark" content="手机版二维码" placement="bottom">
+        <iep-button>
+          <a-icon type="scan" />
+        </iep-button>
+      </el-tooltip>
+    </el-badge>
+  </el-popover>
+</template>
+<script>
+import { openWindow } from '@/util/util'
+export default {
+  methods: {
+    handleOpen () {
+      openWindow('/app/resource/material/material_detail/7914', '国脉内网使用指南', 1400, 900)
+    },
+  },
+}
+</script>
+
+<style lang="css" scoped>
+.item >>> .el-badge__content.is-fixed {
+  z-index: 1;
+  top: 20px;
+  right: 22px;
+}
+</style>
+<style lang="scss" scoped>
+button {
+  border: 0;
+  height: 60px;
+  i {
+    font-size: 20px;
+  }
+  &:focus,
+  &:hover {
+    background-color: #eee;
+    color: #333;
+  }
+}
+</style>
--- src/page/index/top/index.vue
+++ src/page/index/top/index.vue
@@ -23,6 +23,7 @@
     </div>
     <div class="top-right">
       <top-search-icon></top-search-icon>
+      <top-scan></top-scan>
       <top-guide></top-guide>
       <top-message></top-message>
       <top-email></top-email>
@@ -36,6 +37,7 @@ import TopNavbar from './TopNavbar'
 import TopMessage from './TopMessage'
 import TopEmail from './TopEmail'
 import TopGuide from './TopGuide'
+import TopScan from './TopScan'
 import TopSearchIcon from './TopSearchIcon'
 import TopUser from './TopUser'
 export default {
@@ -45,6 +47,7 @@ export default {
     TopEmail,
     TopSearchIcon,
     TopGuide,
+    TopScan,
     TopUser,
   },
   name: 'Top',

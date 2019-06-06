9f50cbbb8a38e08b34ff6e2165fc9ab4018ee4f9
bin245 <245342510@qq.com>
2019-06-06 17:42:40 +0800

    fix(试题库管理): 修复批量删除及查询内容功能

--- src/views/exam/reviewCenter/testQuestionsLibrary/Page/AdvanceSearch.vue
+++ src/views/exam/reviewCenter/testQuestionsLibrary/Page/AdvanceSearch.vue
@@ -3,13 +3,13 @@
     <el-form-item label="内容：">
       <el-input v-model="form.title" clearable></el-input>
     </el-form-item>
-    <el-form-item label="难度">
-      <el-select v-model="form.questionType" placeholder="请选择难度" clearable>
-        <el-option :value="0" label="简单"></el-option>
+    <!-- <el-form-item label="难度">
+      <el-select v-model="form.difficulty" placeholder="请选择难度" clearable>
+        <el-option :value=9 label='简单'></el-option>
         <el-option :value="1" label="一般"></el-option>
         <el-option :value="2" label="困难"></el-option>
       </el-select>
-    </el-form-item>
+    </el-form-item> -->
     <!-- <el-form-item label="分类">
       <el-select v-model="form.organization" placeholder="请选择分类">
         <el-option label="全部"></el-option>
@@ -25,7 +25,7 @@
     </el-form-item> -->
     <el-form-item>
       <iep-button type="primary" @click="searchPage" style="margin:0 10px;">搜索</iep-button>
-      <iep-button style="margin:0 10px;">取消</iep-button>
+      <!-- <iep-button style="margin:0 10px;">取消</iep-button> -->
     </el-form-item>
   </el-form>
 </template>

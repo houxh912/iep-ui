8e35a32118af1cba7cf83b87fa463a5c91553d46
KSSSU <ksu570@gmail.com>
2019-06-11 17:11:57 +0800

    fix(创建新试题): 修复我的考试创建新试题空白页的问题

--- src/views/exam/reviewCenter/testQuestionsLibrary/Page/Edit.vue
+++ src/views/exam/reviewCenter/testQuestionsLibrary/Page/Edit.vue
@@ -4,53 +4,34 @@
     <el-form :model="form" ref="form" label-width="110px" :rules="rules">
       <div class="select">
         <el-form-item class="item" label="题型：" prop="questionType">
-          <el-select v-model="form.questionType" size="small" clearable @change="handleChangeQuestionType" :disabled="questionTypeDisabled">
-            <el-option
-                v-for="(item, index) in res.exms_question_type"
-                :key="index"
-                :label="item.label"
-                :value="item.id"
-              ></el-option>
+          <el-select v-model="form.questionType" size="small" clearable @change="handleChangeQuestionType"
+            :disabled="questionTypeDisabled">
+            <el-option v-for="(item, index) in res.exms_question_type" :key="index" :label="item.label"
+              :value="item.id"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item class="item" label="难度：" prop="difficulty" style="margin-left:20%;">
           <el-select v-model="form.difficulty" size="small" clearable>
-            <el-option
-                v-for="(item, index) in res.exms_difficulty"
-                :key="index"
-                :label="item.label"
-                :value="item.id"
-              ></el-option>
+            <el-option v-for="(item, index) in res.exms_difficulty" :key="index" :label="item.label"
+              :value="item.id"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item class="item" label="科目：" prop="field">
           <el-select v-model="form.field" size="small" clearable>
-            <el-option
-                v-for="(item, index) in res.exms_subjects"
-                :key="index"
-                :label="item.label"
-                :value="item.id"
-              ></el-option>
+            <el-option v-for="(item, index) in res.exms_subjects" :key="index" :label="item.label"
+              :value="item.id"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item class="item" label="题类：" prop="kind" style="margin-left:20%;">
           <el-select v-model="form.kind" size="small" clearable>
-            <el-option
-                v-for="(item, index) in res.exms_question_category"
-                :key="index"
-                :label="item.label"
-                :value="item.id"
-              ></el-option>
+            <el-option v-for="(item, index) in res.exms_question_category" :key="index" :label="item.label"
+              :value="item.id"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item class="item" label="关联：" prop="associatedState">
           <el-select v-model="form.associatedState" size="small" clearable>
-            <el-option
-              v-for="(item, index) in associatedStateList"
-              :key="index"
-              :label="item.label"
-              :value="item.id"
-            ></el-option>
+            <el-option v-for="(item, index) in associatedStateList" :key="index" :label="item.label"
+              :value="item.id"></el-option>
           </el-select>
         </el-form-item>
 
@@ -84,11 +65,11 @@ import mixins from '@/mixins/mixins'
 import SingleDialog from './Single.vue'
 import BatchDialog from './Batch.vue'
 import MutiplyTagSelect from '@/components/deprecated/mutiply-tag-select'
-import { getTestOption,postNewTest,getExamMsg } from '@/api/exam/createExam/newTest/newTest'
+import { getTestOption, postNewTest, getExamMsg } from '@/api/exam/createExam/newTest/newTest'
 export default {
   name: 'report',
-  mixins: [ mixins ],
-  components:{
+  mixins: [mixins],
+  components: {
     SingleDialog,
     BatchDialog,
     MutiplyTagSelect,
@@ -122,18 +103,18 @@ export default {
       },
       tabName: 'Single',
       associatedStateList: [
-        {id: 0,label: '不限'},
-        {id: 1,label: '限考试'},
+        { id: 0, label: '不限' },
+        { id: 1, label: '限考试' },
       ],
       tabList: [
         {
           label: '单题输入',
           value: 'Single',
-        }, 
+        },
         {
           label: '批量导入',
           value: 'Batch',
-        }, 
+        },
       ],
       rules: {
         field: [
@@ -148,61 +129,64 @@ export default {
         difficulty: [
           { required: true, message: '请选择难度', trigger: 'change' },
         ],
-        associatedState:[
+        associatedState: [
           { required: true, message: '请选择关联', trigger: 'change' },
         ],
       },
     }
   },
-  created (){
-    this.getTestOption ()
+  created () {
+    this.getTestOption()
   },
   watch: {
     res: function () {
-      this.getTestPaper ()
+      // this.getTestPaper ()
     },
   },
-  methods:{
+  methods: {
     /**
      * 获取试题
      */
     getTestPaper () {
-      const param ={
-        id: this.record.id,
+      const { id } = this.record
+      if (id) {
+        const param = {
+          id: this.record.id,
+        }
+        getExamMsg(param).then(res => {
+          this.questionTypeDisabled = true
+          this.form = res.data.data
+          this.$refs.single.ruleForm.title = res.data.data.title
+          this.$refs.single.ruleForm.analysis = res.data.data.analysis
+          this.postAnswer = res.data.data.questionType
+          if (res.data.data.questionType == 13) {
+            var arrRadio = JSON.parse(res.data.data.titleOptions)
+            // console.log(arrRadio)
+            this.$refs.single.ruleForm.radioOptions = arrRadio
+            this.$refs.single.ruleForm.radioOption = arrRadio.map(i => i.value)
+            this.$refs.single.ruleForm.inputRadioAnswer = res.data.data.answer
+          }
+          if (res.data.data.questionType == 12) {
+            var arrCheckbox = JSON.parse(res.data.data.titleOptions)
+            // console.log(arrCheckbox)
+            this.$refs.single.ruleForm.checkboxOptions = arrCheckbox
+            this.$refs.single.ruleForm.checkboxOption = arrCheckbox.map(i => i.value)
+            this.$refs.single.ruleForm.inputCheckboxAnswer = JSON.parse(res.data.data.answer)
+          }
+          if (res.data.data.questionType == 11) {
+            this.$refs.single.ruleForm.inputJudgeAnswer = res.data.data.answer
+          }
+          if (res.data.data.questionType == 10) {
+            this.$refs.single.ruleForm.inputShortAnswer = res.data.data.answer
+          }
+          // console.log('res.data.data => ',res.data.data)
+        })
       }
-      getExamMsg(param).then( res => {
-        this.questionTypeDisabled = true
-        this.form = res.data.data
-        this.$refs.single.ruleForm.title = res.data.data.title
-        this.$refs.single.ruleForm.analysis = res.data.data.analysis
-        this.postAnswer = res.data.data.questionType
-        if (res.data.data.questionType == 13) {
-          var arrRadio = JSON.parse(res.data.data.titleOptions)
-          // console.log(arrRadio)
-          this.$refs.single.ruleForm.radioOptions = arrRadio
-          this.$refs.single.ruleForm.radioOption = arrRadio.map(i => i.value)
-          this.$refs.single.ruleForm.inputRadioAnswer = res.data.data.answer
-        }
-        if (res.data.data.questionType == 12) {
-          var arrCheckbox = JSON.parse(res.data.data.titleOptions)
-          // console.log(arrCheckbox)
-          this.$refs.single.ruleForm.checkboxOptions = arrCheckbox
-          this.$refs.single.ruleForm.checkboxOption = arrCheckbox.map(i => i.value)
-          this.$refs.single.ruleForm.inputCheckboxAnswer = JSON.parse(res.data.data.answer)
-        }
-        if (res.data.data.questionType == 11) {
-          this.$refs.single.ruleForm.inputJudgeAnswer = res.data.data.answer
-        }
-        if (res.data.data.questionType == 10) {
-          this.$refs.single.ruleForm.inputShortAnswer = res.data.data.answer
-        }
-        // console.log('res.data.data => ',res.data.data)
-      })
     },
     /**
      *判断题型
      */
-    handleChangeQuestionType (val){
+    handleChangeQuestionType (val) {
       // this.$refs.single.$refs['ruleForm'].resetFields ()
       this.postAnswer = val
     },
@@ -219,15 +203,15 @@ export default {
     /**
      * 提交试题
      */
-    submitSingle (){
+    submitSingle () {
       this.$refs.form.validate((valid) => {
         if (valid) {
-          if(this.$refs.single.submitForm () == true){
+          if (this.$refs.single.submitForm() == true) {
             // this.$refs.single.submitForm ()
             this.form.tag = this.form.tagKeyWords
             let form = this.form
             let ruleForm = this.$refs.single.ruleForm
-            let singleBothForm = Object.assign(form,ruleForm)
+            let singleBothForm = Object.assign(form, ruleForm)
             delete singleBothForm.options
             delete singleBothForm.tagKeyWords
             let postSingleBothForm = {
@@ -237,7 +221,7 @@ export default {
             postSingleBothForm.itemBankList.push(singleBothForm)
             postSingleBothForm.tag = this.form.tag
             postSingleBothForm = JSON.stringify(postSingleBothForm)
-            postNewTest(postSingleBothForm).then( res => {
+            postNewTest(postSingleBothForm).then(res => {
               if (res.data.data == true) {
                 this.$message({
                   type: 'success',
@@ -246,13 +230,13 @@ export default {
                 this.$emit('onGoBack')
               }
             })
-            
+
             // console.log(postSingleBothForm)
             // this.$router.push('/exam/reviewCenter/testQuestionsLibrary/')
           }
         }
       })
-      
+
     },
     /**
      * 获取试题数据
@@ -267,6 +251,7 @@ export default {
         ],
       }
       const { data } = await getTestOption(params)
+      this.getTestPaper()
 
       // const { data } = await getTestOption ('exms_subjects')
       this.res = data

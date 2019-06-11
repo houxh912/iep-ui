269867e803a734c60ce04d824944a2feb8d9283a
KSSSU <ksu570@gmail.com>
2019-06-11 11:43:28 +0800

    fix(专题考试): 状态变更

--- src/views/exam/testPage/subjectTest/Page/examShow.vue
+++ src/views/exam/testPage/subjectTest/Page/examShow.vue
@@ -32,7 +32,19 @@
                       </div>
                       <div class="message" style="overflow: hidden; padding-bottom: 10px;">
                         <span class="title" style="font-size:15px;font-weight:bold;">{{item.title}}</span>
-                        <div class="detail" v-if="item.examStatus === 5" style="margin-bottom: 4px">
+
+                        <div class="detail" style="margin-bottom: 4px">
+                          <template v-if="item.examStatus === 5">
+                            <span style="float:left;width: 40%;">报名日期</span>
+                            <span class="timeShow">{{registDate(item)}}</span>
+                          </template>
+                          <template v-else>
+                            <span style="float:left;width: 40%;">考试日期</span>
+                            <span class="timeShow">{{examDate(item)}}</span>
+                          </template>
+                        </div>
+
+                        <!-- <div class="detail" v-if="item.examStatus === 5" style="margin-bottom: 4px">
                           <span style="float:left;width: 40%;">报名日期</span>
                           <span class="timeShow">{{registDate(item)}}</span>
                         </div>
@@ -41,7 +53,7 @@
                           style="margin-bottom: 4px">
                           <span style="float:left;width: 40%;">考试日期</span>
                           <span class="timeShow">{{examDate(item)}}</span>
-                        </div>
+                        </div> -->
 
                         <!-- <div v-if="item.examStatus !== 1 && item.examStatus !== 2 && item.examStatus !== 3 && item.examStatus !== 4"
                           class="title" style="float:left;">已有 {{item.totalEnrollment}} 人报名</div>
@@ -49,7 +61,7 @@
                           style="float:left;">已有 {{item.totalExam}} 人完成考试</div> -->
 
                         <div class="detail" style="float:left">
-                          <span v-if="item.examStatus === 2 || item.examStatus === 3 || item.examStatus === 4 || item.examStatus === 8">
+                          <span v-if="item.examStatus === 2 || item.examStatus === 3 || item.examStatus === 4 || item.examStatus === 7 || item.examStatus === 8">
                             <em>{{item.totalExam}}</em>人完成
                           </span>
                           <span v-else><em>{{item.totalEnrollment}}</em>人报名</span>
@@ -90,14 +102,14 @@
                       </div>
                       <div class="handleButton">
                         <iep-button type="primary" @click="handleStart(item)" v-if="item.status === 1 && item.examStatus === 7">开始考试</iep-button>
-                        <iep-button type="primary" @click="handleSign(item)" v-if="item.status === 0 && item.examStatus === 5">开始报名</iep-button>
-                        <iep-button type="primary" disabled v-if="item.status !== 1 && item.examStatus === 7 ">等待考试</iep-button>
-                        <iep-button type="primary" disabled v-if="item.status === 2 && item.examStatus === 5">报名结束</iep-button>
-                        <iep-button type="primary" disabled v-if="item.examStatus === 4">考试结束</iep-button>
-                        <iep-button type="primary" disabled v-if="item.examStatus === 2">考试完成</iep-button>
-                        <iep-button type="primary" disabled v-if="item.examStatus === 0">报名审核中</iep-button>
-                        <iep-button type="primary" disabled v-if="item.examStatus === 1 && item.status === 1">报名审核不通过</iep-button>
-                        <iep-button type="primary" disabled v-if="item.examStatus === 3">已交卷</iep-button>
+                        <iep-button type="success" @click="handleSign(item)" v-if="item.status === 0 && item.examStatus === 5">开始报名</iep-button>
+                        <iep-button type="warning" disabled v-if="item.status !== 1 && item.examStatus === 7 ">等待考试</iep-button>
+                        <iep-button type="info" disabled v-if="item.status === 2 && item.examStatus === 5">报名结束</iep-button>
+                        <iep-button type="info" disabled v-if="item.examStatus === 4">考试结束</iep-button>
+                        <iep-button type="info" disabled v-if="item.examStatus === 2">考试完成</iep-button>
+                        <iep-button type="warning" disabled v-if="item.examStatus === 0">报名审核中</iep-button>
+                        <iep-button type="danger" disabled v-if="item.examStatus === 1 && item.status === 1">报名审核不通过</iep-button>
+                        <iep-button type="info" disabled v-if="item.examStatus === 3">已交卷</iep-button>
                         <iep-button type="primary" v-if="item.examStatus === 8">查看成绩</iep-button>
                       </div>
                     </div>
@@ -390,7 +402,7 @@ export default {
   position: absolute;
   width: 10px;
   height: 10px;
-  background: yellow;
+  background: #67c23a;
   -moz-border-radius: 5px;
   -webkit-border-radius: 5px;
   border-radius: 5px;

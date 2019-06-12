ba0bc8a9df1486d6e26e0d3ec463d3b5dbd0ad91
huzhengw115 <huzhengw115@qq.com>
2019-06-12 17:24:31 +0800

    feat(频道页): 紧缺岗位高度问题

--- src/views/app/person/GovPersonContent/ShortagePosts.vue
+++ src/views/app/person/GovPersonContent/ShortagePosts.vue
@@ -4,7 +4,7 @@
       <div class="important" slot="right">
         <el-button type="text" @click="() => {this.$router.push('/app/tobeBole')}">争做伯乐</el-button>
       </div>
-      <el-table :data="tableData" style="width: 100%">
+      <el-table :data="tableData" style="width: 100%" height="300px">
         <el-table-column prop="positionName" label="岗位名称" width="180">
         </el-table-column>
         <el-table-column prop="recruitsCount" label="需求数" width="180">

84c05676eab201750a9b9ea677db1bb6717dd9a6
张超 <zhangchao@126.com>
2019-06-18 11:43:43 +0800

    fix(集团借款): 完成还款操作

--- src/views/fams/OrgBorrow/Components/UnionBorrowDetail/FourthContent.vue
+++ src/views/fams/OrgBorrow/Components/UnionBorrowDetail/FourthContent.vue
@@ -1,6 +1,6 @@
 <template>
   <iep-result type="success" :description="description" :title="title">
-    <el-form class="content-wrapper" ref="form" size="small" :model="data" label-width="150px" disabled>
+    <el-form class="content-wrapper" ref="form" size="small" :model="data" label-width="150px">
       <iep-form-item label-name="支付方式">
         <iep-div-detail :value="dictsMap.borrowMoneyType[data.borrowMoneyType]"></iep-div-detail>
       </iep-form-item>
@@ -31,19 +31,32 @@
       <iep-form-item label-name="借款金额">
         <iep-div-detail :value="`${data.amount}元`"></iep-div-detail>
       </iep-form-item>
+      <template v-if="!data.isOut && data.status === 3 && isRepayment">
+        <iep-divider />
+        <iep-form-item label-name="还款公司">
+          <iep-select v-model="borrowInRepayCompanyId" autocomplete="off" prefix-url="fams/company" placeholder="请选择收入公司"></iep-select>
+        </iep-form-item>
+        <iep-form-item v-if="!repayBankAmountOption.disabled" label-name="还款账户">
+          <iep-select v-model="borrowInRepayCompanyBankId" autocomplete="off" :prefix-url="repayBankAmountOption.prefixUrl" placeholder="请选择银行账户"></iep-select>
+        </iep-form-item>
+      </template>
     </el-form>
     <template v-slot:action>
+      <a-button v-if="!data.isOut && data.status === 3 && isRepayment" type="primary" @click="handleRepay">还款</a-button>
+      <a-button v-if="data.isOut && data.status === 10 && isRepayment" type="primary" @click="handleConfirm">确认收款</a-button>
+
       <a-button v-if="isApproval" type="primary" @click="handleAdd">去填写资金调拨</a-button>
-      <a-button v-if="!isApproval && data.isOut" type="primary" @click="handleFinishAccount">转账完成</a-button>
+
+      <a-button v-if="!isApproval && data.isOut && data.status===1" type="primary" @click="handleFinishAccount">转账完成</a-button>
+      <a-button v-if="!isApproval && !data.isOut && data.status===2" type="primary" @click="handleConfirmAccount">确认收款</a-button>
+
       <a-button style="margin-left: 8px" @click="handleBack">返回列表</a-button>
     </template>
   </iep-result>
 </template>
 <script>
-import {
-  finishFundTransferById,
-  // confirmFundTransferById
-} from '@/api/fams/fund_transfer'
+import { repayOrgBorrow, confirmRepayOrgBorrow } from '@/api/fams/org_borrow'
+import { finishFundTransferById, confirmFundTransferById } from '@/api/fams/fund_transfer'
 import { dictsMap } from './options'
 export default {
   props: ['data'],
@@ -53,11 +66,16 @@ export default {
       title: '操作成功',
       description: '集团财政资金调拨审核通过，等待其调拨。',
       submitLoading: false,
+      borrowInRepayCompanyId: '',
+      borrowInRepayCompanyBankId: '',
     }
   },
   computed: {
     isApproval () {
       return this.$route.query.approval === 'true'
+    },
+    isRepayment () {
+      return this.$route.query.repayment === 'true'
     },
     bankAmountOption () {
       if (this.data.borrowInCompanyId && this.data.borrowMoneyType === '1') {
@@ -72,22 +90,96 @@ export default {
         }
       }
     },
+    repayBankAmountOption () {
+      if (this.borrowInRepayCompanyId) {
+        return {
+          disabled: false,
+          prefixUrl: `fams/bank_account/${this.borrowInRepayCompanyId}`,
+        }
+      } else {
+        return {
+          disabled: true,
+          prefixUrl: `fams/bank_account/${this.borrowInRepayCompanyId}`,
+        }
+      }
+    },
   },
   created () {
   },
   methods: {
+    async handleConfirm () {
+      try {
+        await this.$confirm('此操作将已收款, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning',
+        })
+        this.submitLoading = true
+        try {
+          const { data } = await confirmRepayOrgBorrow(this.data.id)
+          if (data.data) {
+            this.$message('操作成功')
+            this.handleBack()
+          } else {
+            this.$message(data.msg)
+          }
+        } catch (error) {
+          this.$message('似乎出现了一些问题')
+        }
+      } catch (error) {
+        this.$message('操作取消')
+      } finally {
+        this.submitLoading = false
+      }
+    },
+    async handleRepay () {
+      try {
+        await this.$confirm('此操作将还款, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning',
+        })
+        this.submitLoading = true
+        try {
+          const { data } = await repayOrgBorrow({
+            id: this.data.id,
+            borrowInRepayCompanyId: this.borrowInRepayCompanyId,
+            borrowInRepayCompanyBankId: this.borrowInRepayCompanyBankId,
+          })
+          if (data.data) {
+            this.$message('操作成功')
+            this.handleBack()
+          } else {
+            this.$message(data.msg)
+          }
+        } catch (error) {
+          this.$message('似乎出现了一些问题')
+        }
+      } catch (error) {
+        this.$message('操作取消')
+      } finally {
+        this.submitLoading = false
+      }
+    },
+    handleConfirmAccount () {
+      this._handleConfirm(this.data.id, confirmFundTransferById, '收款')
+    },
     handleFinishAccount () {
-      this.$confirm('是否确认', '提示', {
+      this._handleConfirm(this.data.id, finishFundTransferById, '转账')
+    },
+    _handleConfirm (id, functionName, msg) {
+      this.$confirm(`是否确认${msg}`, '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning',
       }).then(async () => {
-        const { data } = await finishFundTransferById(this.data.id)
+        const { data } = await functionName(id)
         if (data.data) {
           this.$message({
             type: 'success',
             message: '操作成功!',
           })
+          this.handleBack()
         } else {
           this.$message({
             type: 'info',
--- src/views/fams/OrgBorrow/Components/UnionBorrowDetail/options.js
+++ src/views/fams/OrgBorrow/Components/UnionBorrowDetail/options.js
@@ -10,6 +10,9 @@ const dictsMap = {
 const statusMap = {
   '0': 2,
   '1': 3,
+  '2': 3,
+  '3': 3,
+  '10': 3,
 }
 
 const initForm = (config) => {

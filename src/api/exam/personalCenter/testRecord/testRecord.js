f1f4a648cae9b53f8bd12b9c993a72b1774bfcee
KSSSU <ksu570@gmail.com>
2019-06-21 12:06:20 +0800

    feat(专题考试): 查看证书

--- src/api/exam/personalCenter/testRecord/testRecord.js
+++ src/api/exam/personalCenter/testRecord/testRecord.js
@@ -8,9 +8,21 @@ import request from '@/router/axios'
  * @param {Object} params 参数
  */
 export function getTestRecordList (params) {
-    return request({
-        url: '/exms/iepexamination/selectById',
-        method: 'get',
-        params: params,
-    })
+  return request({
+    url: '/exms/iepexamination/selectById',
+    method: 'get',
+    params: params,
+  })
+}
+
+/**
+ * 获取专题考试列表页
+ * @param {Object} params 参数
+ */
+export function getCertificate (params) {
+  return request({
+    url: '/exms/iepcertificate/grantimage',
+    method: 'get',
+    params: params,
+  })
 }

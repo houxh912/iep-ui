9fb06b051bed5832940d2a882d3d7a5a3c432021
の半梦 <835806940@qq.com>
2019-06-21 11:13:00 +0800

    feat(组织列表细节): 组织列表细节
    组织列表细节

--- src/views/app/organizationalList/OrganizationalCon.vue
+++ src/views/app/organizationalList/OrganizationalCon.vue
@@ -212,14 +212,13 @@ export default {
       display: flex;
       justify-content: flex-start;
       align-items: center;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
       span {
         position: relative;
         padding: 0 8px;
         line-height: 14px;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        max-width: 80px;
         &::before {
           position: absolute;
           content: "";

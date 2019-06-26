cd45e8c5dacad42da1165814d6c003682d9bf7f8
の半梦 <835806940@qq.com>
2019-06-26 11:19:05 +0800

    feat(列表页修改说说页面修改): 说说列表页修改
    说说列表页修改

---
+++ src/views/app/moreThoughts/Search.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="expert-search">
+    <h3>{{title}}<span>{{num}}</span></h3>
+    <div class="search-con">
+      <operation-search @search-page="searchPage"></operation-search>
+      <el-button type="danger" plain>高级搜索</el-button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      title: '说说',
+      num: '（共9865个）',
+    }
+  },
+  methods: {
+    searchPage (val) {
+      this.$emit('load-page', val)
+    },
+  },
+}
+</script>
+<style scoped  lang="scss">
+.expert-search {
+  margin: 0 auto 25px;
+  text-align: center;
+  h3 {
+    font-size: 24px;
+    span {
+      font-size: 14px;
+      color: #999;
+    }
+  }
+}
+.search-con {
+  display: flex;
+  margin: 0 auto;
+  max-width: 600px;
+  justify-content: space-between;
+}
+</style>
+<style scoped>
+.expert-search >>> .el-card__body {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+.expert-search >>> .el-input--small input {
+  height: 40px !important;
+}
+.expert-search >>> .el-card {
+  background-color: #fafafa;
+}
+.expert-search >>> .search-con .input-wrapper {
+  max-width: 510px;
+  width: 510px;
+}
+.expert-search >>> .el-button--danger.is-plain {
+  color: #cb3737;
+  background: #fef0f0;
+  border-color: #cb3737;
+}
+.expert-search >>> .el-button--danger.is-plain:hover,
+.expert-search >>> .el-button--danger.is-plain:focus {
+  background: #cb3737;
+  border-color: #ba1b21;
+  color: #fff;
+}
+</style>

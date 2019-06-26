9017a988f8ee2c4c1c924115da9ec993d5706271
bin245 <245342510@qq.com>
2019-06-26 14:33:12 +0800

    fix(考试库): 修复输入关键词搜索后点击第二页显示错误数据问题

--- src/views/exam/reviewCenter/examLibrary/examination/List.vue
+++ src/views/exam/reviewCenter/examLibrary/examination/List.vue
@@ -190,7 +190,7 @@ export default {
       return row.creatorId === this.userInfo.userId
     },
 
-    loadPage (param) {
+    loadPage (param = this.searchForm) {
       this.loadTable(param, getExamInationList)
     },
     /**

41961de2ccc8e73ae1322b37c13afc77c02b66f7
张超 <zhangchao@126.com>
2019-06-26 15:41:21 +0800

    fix(fix): fix

--- src/views/fams/FinancialManagement/IManagement/index.vue
+++ src/views/fams/FinancialManagement/IManagement/index.vue
@@ -12,6 +12,13 @@
         </template>
       </operation-container>
       <iep-table :isLoadTable="isLoadTable" :pagination="pagination" :dictsMap="dictsMap" :columnsMap="columnsMap" :pagedTable="pagedTable" @size-change="handleSizeChange" @current-change="handleCurrentChange" @row-click="handleDetail" :cell-style="mixinsCellPointerStyle">
+        <!-- <el-table-column prop="operation" label="操作" width="100" fixed="right">
+          <template slot-scope="scope">
+            <operation-wrapper>
+              <iep-button v-if="scope.row.projectStatus===2" @click="handleProcess(scope.row)">待处理</iep-button>
+            </operation-wrapper>
+          </template>
+        </el-table-column> -->
       </iep-table>
     </basic-container>
     <dialog-form ref="DialogForm" @load-page="loadPage"></dialog-form>

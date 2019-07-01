0c5bfca745f87305710b55dbf3896b7c86ca562e
gold <291817075@qq.com>
2019-07-01 09:27:25 +0800

    fix(通讯录修复): 通讯录修复

--- src/views/wel/RelationshipManage/index.vue
+++ src/views/wel/RelationshipManage/index.vue
@@ -7,14 +7,12 @@
             <span class="title">通讯录</span>
           </div>
           <el-menu :default-openeds="selectType" class="menu-vertical">
-            <!-- <el-submenu index="1" collapse>
-              <template slot="title">
-                <span>国脉人</span>
-              </template>
-              <el-menu-item class="menu-item" :index="item.value+''" :key="item.value" v-for="item in allPeople" @click.native="handleAllPeople(item.value)">
+            <el-menu-item index="1" @click.native="handleAllPeople(1001)">
+              <span slot="title">国脉人</span>
+              <!-- <el-menu-item class="menu-item" :index="item.value+''" :key="item.value" v-for="item in allPeople" @click.native="handleAllPeople(item.value)">
                 <span>{{item.label}}</span>
-              </el-menu-item>
-            </el-submenu> -->
+              </el-menu-item> -->
+            </el-menu-item>
             <el-submenu index="2" collapse>
               <template slot="title">
                 <span>我的关系</span>

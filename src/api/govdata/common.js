97895bbd229c4e3a078598d5c2b788899f5f6f8c
KSSSU <ksu570@gmail.com>
2019-07-12 14:21:28 +0800

    fix(国策): 国策代理接口从/gov换成/gc，

--- src/api/govdata/common.js
+++ src/api/govdata/common.js
@@ -1,51 +1,51 @@
 import request from '@/router/axios'
 
-const prefixUrl = '/gov'
+const prefixUrl = '/gc'
 
 // 查看通用政策和申报政策的政策依据或政策原文
 export function getBasisPage (params) {
-    return request({
-        url: `${prefixUrl}/policy/general/getGeneralAndDeclareList`,
-        method: 'post',
-        params: params,
-        headers: {
-          isNoNeed: true,
-      },
-    })
-}
-
-// 查看机构分页
-export function getOrganizationPage (params) {
-    return request({
-      url: `${prefixUrl}/organization/page`,
-      method: 'get',
+  return request({
+      url: `${prefixUrl}/policy/general/getGeneralAndDeclareList`,
+      method: 'post',
       params: params,
       headers: {
         isNoNeed: true,
     },
-    })
-  }
+  })
+}
+
+// 查看机构分页
+export function getOrganizationPage (params) {
+  return request({
+    url: `${prefixUrl}/organization/page`,
+    method: 'get',
+    params: params,
+    headers: {
+      isNoNeed: true,
+    },
+  })
+}
 
 // 移动政策
 export function movePolicy (params) {
   return request({
-    url: '/gov/mobile/policy',
+    url: `${prefixUrl}/mobile/policy`,
     method: 'post',
     params: params,
     headers: {
       isNoNeed: true,
-  },
+    },
   })
 }
 
 //获取层级、适用对象、主题、规模、行业
 export function findByTypeList () {
   return request({
-      url: '/admin/dict/all_map',
-      method: 'get',
-      //params: {numberList},
-      headers: {
-        isNoNeed: true,
+    url: '/admin/dict/all_map',
+    method: 'get',
+    //params: {numberList},
+    headers: {
+      // isNoNeed: true,
     },
   })
 }

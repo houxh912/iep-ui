bd802fa99f4c7c7384ae373f2bc07598939ad110
gold <291817075@qq.com>
2019-07-13 15:01:00 +0800

    fix(a): a

--- src/views/gpms/international/index.vue
+++ src/views/gpms/international/index.vue
@@ -1,3 +1,73 @@
 <template>
-  <div></div>
+  <basic-container>
+    <iep-page-header title="项目公海"></iep-page-header>
+    <operation-container>
+      <template slot="left">
+      </template>
+      <template slot="right">
+        <operation-search @search-page="searchPage" advance-search placeHolder="请输入项目名称" prop="projectName">
+        </operation-search>
+      </template>
+    </operation-container>
+    <iep-table :isLoadTable="isLoadTable" :pagination="pagination" :columnsMap="columnsMap" :dictMap="dictMap" :pagedTable="pagedTable" @size-change="handleSizeChange" @current-change="handleCurrentChange">
+      <el-table-column label="项目名称" slot="before-columns" width="300px">
+        <template slot-scope="scope">
+          <div style="cursor: pointer;width: 100%;" @click="handleDetail(scope.row)">
+            <span>{{ scope.row.projectName }}</span>
+          </div>
+        </template>
+      </el-table-column>
+      <el-table-column label="立项时间" width="150px">
+        <template slot-scope="scope">
+          <span>{{ scope.row.approvalTime || parseToDay }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="项目状态">
+        <template slot-scope="scope">
+          <span v-if="scope.row.projectStatus==1">待提交</span>
+          <span v-if="scope.row.projectStatus==2">待审核</span>
+          <span v-if="scope.row.projectStatus==3">已立项</span>
+          <span v-if="scope.row.projectStatus==4">审核未通过</span>
+          <span v-if="scope.row.projectStatus==5">锁定</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" width="200px">
+        <template>
+          <operation-wrapper>
+          </operation-wrapper>
+        </template>
+      </el-table-column>
+    </iep-table>
+  </basic-container>
 </template>
+<script>
+import mixins from '@/mixins/mixins'
+import { getProjectPage } from '@/api/gpms/fas'
+import { columnsMap, dictMap } from './option'
+
+export default {
+  mixins: [mixins],
+  data () {
+    return {
+      // replaceText: (data) => `[共${data[0]}条数据]`,
+      columnsMap,
+      dictMap,
+    }
+  },
+  created () {
+    this.loadPage()
+  },
+  methods: {
+    async loadPage (params = {}) {
+      await this.loadTable(Object.assign({}, params, this.searchForm), getProjectPage)
+      // this.statistics = data.total
+    },
+    searchPage (val) {
+      this.searchForm = Object.assign({}, this.searchForm, val)
+      this.loadPage()
+    },
+  },
+}
+</script>
+<style scoped>
+</style>
---
+++ src/views/gpms/international/option.js
@@ -0,0 +1,28 @@
+import { getStore } from '@/util/store'
+const dicData = getStore({ name: 'dictGroup' })
+function changeDict (list) {
+  let data = {}
+  for (let item of list) {
+    data[item.value] = item.label
+  }
+  return data
+}
+
+export const dictMap = {
+  projectStage: changeDict(dicData.prms_project_stage),
+}
+
+export const columnsMap = [
+  {
+    prop: 'projectBudget',
+    label: '项目预算',
+  },
+  {
+    prop: 'contractAmount',
+    label: '合同金额',
+  },
+  {
+    prop: 'projectTime',
+    label: '立项时间',
+  },
+]

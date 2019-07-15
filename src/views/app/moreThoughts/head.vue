54ce445ca3f9682c8a8ab2464d8c8d3379b40411
huzhengw115 <huzhengw115@qq.com>
2019-07-15 14:14:10 +0800

    feat(说说新版本): 界面

---
+++ src/views/app/moreThoughts/head.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="head">
+    <div class="title">早晚五分钟，为<span class="akey">智慧</span>加油</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.head {
+  width: 1200px;
+  height: 100px;
+  color: #f8f8f8;
+  text-align: center;
+  .title {
+    font-size: 20px;
+    .akey {
+      color: #c74c50;
+    }
+  }
+}
+</style>

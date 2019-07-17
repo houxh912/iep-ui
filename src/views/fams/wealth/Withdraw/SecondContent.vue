33556c6530adaec6d1d11a94181f8f5d07d071e4
zhangchao <zhangchao@126.com>
2019-07-17 09:54:16 +0800

    feat(组件): 移除ant-design不要的组件,减少打包体积

--- src/views/fams/wealth/Withdraw/SecondContent.vue
+++ src/views/fams/wealth/Withdraw/SecondContent.vue
@@ -1,7 +1,7 @@
 <template>
   <steps-content>
     <a-form class="content-wrapper">
-      <a-alert :closable="true" type="error" message="确认提现后，财务将对您的提现申请进行核准。" style="margin-bottom: 24px;" />
+      <el-alert style="margin-bottom: 24px;" title="确认提现后，财务将对您的提现申请进行核准。" type="warning" show-icon></el-alert>
       <a-form-item label="提现金额：" :labelCol="labelCol" :wrapperCol="wrapperCol" class="stepFormText">
         ￥ {{formatNumber(data.amount)}}
       </a-form-item>

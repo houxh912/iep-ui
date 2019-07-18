7f3c0ecc93cd45a14a1bb287537d5328ae3ff651
zhangchao <zhangchao@126.com>
2019-07-18 10:12:29 +0800

    feat(财务): 完成集团基金资金明细

---
+++ src/views/fams/GroupFinance/CurrencyFund/BankAccount/options.js
@@ -0,0 +1,12 @@
+const columnsMap = [
+  {
+    prop: 'name',
+    label: '银行账户名称',
+  },
+  {
+    prop: 'bankBalance',
+    label: '银行卡余额',
+  },
+]
+
+export { columnsMap }
---
+++ src/views/fams/GroupFinance/CurrencyFund/Company/options.js
@@ -0,0 +1,16 @@
+const columnsMap = [
+  {
+    prop: 'name',
+    label: '公司',
+  },
+  {
+    prop: 'cashBalance',
+    label: '现金余额',
+  },
+  {
+    prop: 'bankBalance',
+    label: '银行余额',
+  },
+]
+
+export { columnsMap }

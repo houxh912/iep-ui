45f6e7a9d05f60beaecc88cbe59cba6090bcffa8
张超 <zhangchao@126.com>
2019-07-20 16:53:48 +0800

    feat(命令): 一键合并

--- release.sh
+++ release.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-echo "Hello World !"
+echo "Start Build New Release Branch!"
+
+git reset --hard origin/develop \
+&& git merge origin/feature/block-chain/dev \
+&& git merge origin/feature/atms/back \
+&& git merge origin/feature/project/new \
+&& git merge origin/feature/im/dev \
+&& git merge origin/exam/fix/dev
+
+echo "Success!"

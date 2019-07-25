25670125efa7d6c2f0ef3f3e04a36f11c3b9438d
zhangchao <zhangchao@126.com>
2019-07-25 15:42:00 +0800

    fix(财富): 完成修复与图标更新

--- src/config/env.js
+++ src/config/env.js
@@ -4,8 +4,8 @@ const env = process.env
 
 const baseUrl = ''
 
-const iconfontVersion = ['1036949_hkqd1wblcjo']
-const multicolorIconfontUrl = '//at.alicdn.com/t/font_1184303_t5v0uj66ok.js'
+const iconfontVersion = ['1036949_aza0i8923yp']
+const multicolorIconfontUrl = '//at.alicdn.com/t/font_1184303_mhqvids4u2k.js'
 const iconfontUrl = '//at.alicdn.com/t/font_$key.css'
 
 const codeUrl = '/api/code'

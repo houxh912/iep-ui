2a1fb6c29440bc84f9c66c776461c9f51567654e
zhangchao <zhangchao@126.com>
2019-08-02 12:29:02 +0800

    feat(登录): 页面重写

---
+++ src/page/Components/IepPageLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app-layout">
+    <div>
+      <slot></slot>
+    </div>
+    <iep-page-footer-bar></iep-page-footer-bar>
+  </div>
+</template>
+<script>
+import IepPageFooterBar from './IepPageFooterBar'
+export default {
+  name: 'IepPageLayout',
+  components: { IepPageFooterBar },
+}
+</script>
+<style lang="css" scoped>
+.app-layout {
+  min-height: 100vh;
+  display: flex;
+  justify-content: space-between;
+  background: url("/img/bg/bg-login.webp") no-repeat;
+  background-size: cover;
+  flex-direction: column;
+}
+</style>
+
+
---
+++ src/page/newlogin/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <iep-page-layout>
+    <top-wrapper>
+      <template slot="top-left">
+        <top-logo></top-logo>
+      </template>
+      <template slot="top-right">
+      </template>
+    </top-wrapper>
+  </iep-page-layout>
+</template>
+<script>
+import TopWrapper from '@/page/Components/TopWrapper'
+import TopLogo from '@/page/Components/TopLogo'
+import IepPageLayout from '@/page/Components/IepPageLayout'
+export default {
+  components: {
+    TopWrapper,
+    TopLogo,
+    IepPageLayout,
+  },
+}
+</script>

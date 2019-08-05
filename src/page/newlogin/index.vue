4dc89e839a6b9fe4b5066390ed15985d35600f1f
zhangchao <zhangchao@126.com>
2019-08-05 11:42:41 +0800

    fix(登录): 动画

--- src/page/newlogin/index.vue
+++ src/page/newlogin/index.vue
@@ -7,6 +7,13 @@
       <template slot="top-right">
       </template>
     </top-wrapper>
+    <div class="login-container-wrapper">
+      <div class="login-img-wrapper">
+        <img v-show="currentId === 0" class="animated fadeInUp" src="/img/bg/wh1.png" alt="">
+        <img v-show="currentId === 1" class="animated fadeInUp" src="/img/bg/wh2.png" alt="">
+        <img v-show="currentId === 2" class="animated fadeInUp" src="/img/bg/wh3.png" alt="">
+      </div>
+    </div>
   </iep-page-layout>
 </template>
 <script>
@@ -19,5 +26,31 @@ export default {
     TopLogo,
     IepPageLayout,
   },
+  data () {
+    return {
+      currentId: 0,
+    }
+  },
+  mounted () {
+    this.animationInterval = setInterval(() => {
+      if (this.currentId === 2) {
+        this.currentId = 0
+      } else {
+        this.currentId++
+      }
+    }, 5000)
+  },
+  destroyed () {
+    clearInterval(this.animationInterval)
+  },
 }
 </script>
+<style lang="scss" scoped>
+.login-container-wrapper {
+  display: flex;
+  align-items: center;
+  .login-img-wrapper {
+    // position: absolute;
+  }
+}
+</style>

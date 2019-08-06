c07257c05ada9aadd3d34cc28eae7d29c9e302e9
zhnagqi <850629074@qq.com>
2019-08-06 16:51:18 +0800

    fix: fix
    fix

--- src/views/conm/DocumentManagement/DialogForm.vue
+++ src/views/conm/DocumentManagement/DialogForm.vue
@@ -21,8 +21,9 @@
           图片：
         </span>
         <!-- <iep-upload v-model="formData.attachFileList" :limit="limit"></iep-upload> -->
-        <el-upload class="avatar-uploader" action="/api/admin/file/upload/avatar" :show-file-list="true" :headers="headers" :on-success="handleAvatarSuccess" :on-remove="handleRemove" accept="image/*">
+        <el-upload class="avatar-uploader" action="/api/admin/file/upload/avatar" :show-file-list="false" :headers="headers" :on-success="handleAvatarSuccess" :on-remove="handleRemove" accept="image/*">
           <iep-img v-if="form.image" :src="form.image" class="avatar"></iep-img>
+          <i v-if="form.image" class="el-icon-circle-close" @click.stop="handleRemove"></i>
           <i v-else class="el-icon-plus avatar-uploader-icon"></i>
         </el-upload>
       </el-form-item>
@@ -89,6 +90,7 @@ export default {
       })
     },
     handleAvatarSuccess (row) {
+      // this.handleRemove()
       this.form.image = row.data.url
     },
     handleRemove () {
@@ -114,7 +116,21 @@ export default {
   border-radius: 6px;
   cursor: pointer;
   position: relative;
-  overflow: hidden;
+  transition: 0.3s;
+  &:hover .el-icon-circle-close {
+    visibility: visible;
+  }
+}
+.el-icon-circle-close {
+  position: absolute;
+  visibility: hidden;
+  top: -7px;
+  right: -7px;
+  font-size: 16px;
+  color: #ccc;
+  &:hover {
+    color: #409eff;
+  }
 }
 .avatar-uploader:hover {
   border-color: #409eff;

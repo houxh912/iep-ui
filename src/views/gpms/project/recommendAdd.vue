0161156b81c366fb0fb794670dbc7aaae63e1517
gold <291817075@qq.com>
2019-08-09 09:11:57 +0800

    fix(a): a

--- src/views/gpms/project/recommendAdd.vue
+++ src/views/gpms/project/recommendAdd.vue
@@ -863,9 +863,9 @@ export default {
       }//平均成本预算
       else if (val == 'contractAmount') {
         getAverageContractAmount({ tagList: this.tagList }).then(({ data }) => {
-          this.chartDataRing2.rows[0].数量 = data.classifiedProportion
-          this.chartDataRing2.rows[1].数量 = data.classifiedProportion1
-          this.chartDataRing2.rows[2].数量 = data.classifiedProportion2
+          this.chartDataRing3.rows[0].数量 = data.classifiedProportion
+          this.chartDataRing3.rows[1].数量 = data.classifiedProportion1
+          this.chartDataRing3.rows[2].数量 = data.classifiedProportion2
           this.chartData.averageAmount = data.averageAmount
         })
       }//平均合同金额字段

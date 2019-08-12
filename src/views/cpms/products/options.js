f5ed690256a88214b955abbfd474fc51c599938c
zhangchao <zhangchao@126.com>
2019-08-12 15:31:51 +0800

    feat(产品): 添加上审核功能

--- src/views/cpms/products/options.js
+++ src/views/cpms/products/options.js
@@ -1,6 +1,26 @@
 import { checkContactUsers } from '@/util/rules'
 
-const columnsMap = []
+const dictsMap = {
+  status: {
+    0: '待审核',
+    1: '通过',
+    2: '未通过',
+  },
+}
+
+const columnsMap = [
+  {
+    label: '上线时间',
+    prop: 'onlineTime',
+    type: 'date',
+    formatString: 'YYYY-MM-DD',
+  },
+  {
+    label: '状态',
+    prop: 'status',
+    type: 'dict',
+  },
+]
 
 const Column = {
   id: '',
@@ -103,4 +123,4 @@ const rules = {
   ],
 }
 
-export { columnsMap, Column, initForm, toDtoForm, initSearchForm, rules }
+export { columnsMap, Column, initForm, toDtoForm, initSearchForm, rules, dictsMap }

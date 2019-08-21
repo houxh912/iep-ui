9a3028f0f9ed8fb8187dedbf0e35818184353927
zhangchao <zhangchao@126.com>
2019-08-21 16:49:11 +0800

    build(add): ican.sh

---
+++ ican.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Start Build New Release Branch!"
+
+git reset --hard origin/i-can \
+&& git merge origin/feature/atms/back \
+&& git merge origin/feature/project/new \
+&& git merge origin/feature/im/dev \
+&& git merge origin/exam/fix/dev \
+&& git merge origin/feature/govdata/dev \
+&& git merge origin/feature/pg-big-data/dev
+
+echo "Success!"

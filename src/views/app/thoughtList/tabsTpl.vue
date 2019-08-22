d628f2062faf7f0a8207e89d3e94105d04412e9d
huzhengw115 <huzhengw115@qq.com>
2019-08-22 15:10:46 +0800

    feat(说说): 搜索功能完成

---
+++ src/views/app/thoughtList/tabsTpl.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="tabs">
+    <el-tabs class="iep-tabs" v-bind="$attrs" v-on="$listeners">
+      <el-tab-pane v-for="tab in tabList" :label="tab.label" :name="tab.value" :key="tab.value" :disabled="tab.disabled">
+        <slot :name="tab.value"></slot>
+      </el-tab-pane>
+    </el-tabs>
+    <div class="search">
+      <slot name="search"></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'IepTabs',
+  inheritAttrs: false,
+  props: {
+    tabList: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data () {
+    return {
+
+    }
+  },
+}
+</script>
+<style lang="scss" scoped>
+.tabs {
+  position: relative;
+  .search {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+}
+.iep-tabs ::v-deep .el-tabs__nav-wrap::after {
+  height: 1px;
+}
+.iep-tabs ::v-deep .el-tabs__active-bar {
+  background-color: $--color-primary;
+}
+.iep-tabs ::v-deep .el-tabs__item.is-active {
+  color: $--color-primary;
+}
+.iep-tabs ::v-deep .el-tabs__item {
+  font-size: 16px;
+  height: 22px;
+  line-height: 22px;
+}
+</style>

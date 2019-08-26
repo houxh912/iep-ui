9e1382f76e0f5a4f74b021f8ec134386fe9ed051
lanseria <zhangchao564265135@hotmail.com>
2019-08-26 14:32:55 +0800

    chore(fix): bash

--- release.sh
+++ release.sh
@@ -3,9 +3,6 @@ echo "Start Build New Release Branch!"
 
 git reset --hard origin/develop \
 && git merge origin/feature/atms/back \
-&& git merge origin/feature/project/new \
-&& git merge origin/feature/im/dev \
-&& git merge origin/exam/fix/dev \
 && git merge origin/feature/govdata/dev \
 && git merge origin/feature/pg-big-data/dev
 

4016efc0c42beb41cd47b80cca52acfe93349bc4
lanseria <zhangchao564265135@hotmail.com>
2019-08-30 14:40:23 +0800

    feat(优化图片): 图片减少体积

--- src/views/app/allianceDetails/mechanism/index.vue
+++ src/views/app/allianceDetails/mechanism/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="mechanism">
     <h3>一、组织架构图</h3>
-    <iep-img class="iep-img" :src="'../../img/app/detail/img_031.png'"></iep-img>
+    <iep-img class="iep-img" :src="'../../img/app/detail/img_031.webp'"></iep-img>
     <h3>二、国脉高层</h3>
-    <iep-img class="iep-img" :src="'../../img/app/detail/img_032.png'"></iep-img>
+    <iep-img class="iep-img" :src="'../../img/app/detail/img_032.webp'"></iep-img>
     <h3>三、国脉机构分布图</h3>
-    <iep-img class="iep-img" :src="'../../img/app/detail/img_033.png'"></iep-img>
+    <iep-img class="iep-img" :src="'../../img/app/detail/img_033.webp'"></iep-img>
   </div>
 </template>
 

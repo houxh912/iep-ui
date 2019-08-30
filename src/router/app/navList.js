f21b61e4527dec7171ca2f31202e9867694ecf66
zhangchao <zhangchao@126.com>
2019-08-30 12:57:02 +0800

    fix(修正): 方便seo

--- src/router/app/navList.js
+++ src/router/app/navList.js
@@ -3,19 +3,19 @@ const navList = [
     id: '//www.icanvip.net/',
     name: '首页',
   }, {
-    id: '//www.icanvip.net/newsList',
+    id: '//www.icanvip.net/newsList/',
     name: '头条新知',
   }, {
-    id: '//www.icanvip.net/businessList',
+    id: '//www.icanvip.net/businessList/',
     name: '商机合作',
   }, {
-    id: '//www.icanvip.net/dataAssets',
+    id: '//www.icanvip.net/dataAssets/',
     name: '数据资产',
   }, {
-    id: '//www.icanvip.net/masterList',
+    id: '//www.icanvip.net/masterList/',
     name: '师父专家',
   }, {
-    id: '//www.icanvip.net/thoughtsList',
+    id: '//www.icanvip.net/thoughtsList/',
     name: '我要说说',
   },
 ]

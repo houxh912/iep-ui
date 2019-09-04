5e848a3f84126ed6113eff1b771e85b9b18a4882
zhangchao <zhangchao@126.com>
2019-09-04 13:43:48 +0800

    fix(fix): url问题

--- src/util/url.js
+++ src/util/url.js
@@ -1,13 +1,7 @@
 export const getLocation = () => {
   if (isUrl(document.domain)) {
-    var arr = document.domain.split('.')
-    if (arr.length === 2) {
-      return document.domain
-    }
-    if (arr.length > 2 && arr[0] !== 'www') {
-      return arr.slice(1).join('.')
-    }
-    return arr.slice(1).join('.')
+    const hostArr = location.host.split('.')
+    return hostArr.length === 2 ? hostArr.join('.') : hostArr.slice(1).join('.')
   }
   return document.domain
 }

73f8bab3179afc3760d02e8631ae1a2ca0b689ac
张超 <zhangchao@126.com>
2019-09-05 22:08:42 +0800

    chore(暂时): 关闭奖惩记录

--- src/views/hrms/GrowthFile/RewardPunishment/index.vue
+++ src/views/hrms/GrowthFile/RewardPunishment/index.vue
@@ -1,46 +1,47 @@
 <template>
-  <iep-table :isLoadTable="isLoadTable" :columnsMap="columnsMap" :pagedTable="pagedTable">
-  </iep-table>
+  <!-- <iep-table :isLoadTable="isLoadTable" :columnsMap="columnsMap" :pagedTable="pagedTable">
+  </iep-table> -->
+  <iep-no-data></iep-no-data>
 </template>
 <script>
-import mixins from '@/mixins/mixins'
-import { getWealthFlowPageByUserId } from '@/api/fams/wealth_flow'
-export default {
-  mixins: [mixins],
-  data () {
-    return {
-      columnsMap: [
-        {
-          prop: 'flowId',
-          label: 'ID',
-        },
-        {
-          prop: 'creatorName',
-          label: '操作者',
-        },
-        {
-          prop: 'amount',
-          label: '金额',
-        },
-        {
-          prop: 'createTime',
-          label: '操作时间',
-        },
-      ],
-    }
-  },
-  computed: {
-    id () {
-      return +this.$route.params.id
-    },
-  },
-  created () {
-    this.loadPage()
-  },
-  methods: {
-    loadPage (param = this.searchForm) {
-      this.loadTable({ ...param, userId: this.id }, getWealthFlowPageByUserId)
-    },
-  },
-}
+// import mixins from '@/mixins/mixins'
+// import { getWealthFlowPageByUserId } from '@/api/fams/wealth_flow'
+// export default {
+//   mixins: [mixins],
+//   data () {
+//     return {
+//       columnsMap: [
+//         {
+//           prop: 'flowId',
+//           label: 'ID',
+//         },
+//         {
+//           prop: 'creatorName',
+//           label: '操作者',
+//         },
+//         {
+//           prop: 'amount',
+//           label: '金额',
+//         },
+//         {
+//           prop: 'createTime',
+//           label: '操作时间',
+//         },
+//       ],
+//     }
+//   },
+//   computed: {
+//     id () {
+//       return +this.$route.params.id
+//     },
+//   },
+//   created () {
+//     this.loadPage()
+//   },
+//   methods: {
+//     loadPage (param = this.searchForm) {
+//       this.loadTable({ ...param, userId: this.id }, getWealthFlowPageByUserId)
+//     },
+//   },
+// }
 </script>

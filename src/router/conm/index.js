a4bf43c4bd822929cad7c8979d2fc8e46cf88f4f
zhnagqi <850629074@qq.com>
2019-09-06 14:45:44 +0800

    fix: fix
    fix

--- src/router/conm/index.js
+++ src/router/conm/index.js
@@ -9,26 +9,26 @@ export default [
         name: '站点管理详情',
         component: () => import('@/views/conm/StationGroupManagement/Detail'),
       },
-      {
-        path: 'column_management/:id',
-        name: '栏目管理',
-        component: () => import('@/views/conm/ColumnManagement/index'),
-      },
-      {
-        path: 'attribute_management/:id',
-        name: '推荐位管理',
-        component: () => import('@/views/conm/AttributeManagement/index'),
-      },
-      {
-        path: 'a_d_slot_management/:id',
-        name: '广告位管理',
-        component: () => import('@/views/conm/ADSlotManagement/index'),
-      },
-      {
-        path: 'a_d_management/:id',
-        name: '广告管理',
-        component: () => import('@/views/conm/ADManagement/index'),
-      },
+      // {
+      //   path: 'column_management/:id',
+      //   name: '栏目管理',
+      //   component: () => import('@/views/conm/ColumnManagement/index'),
+      // },
+      // {
+      //   path: 'attribute_management/:id',
+      //   name: '推荐位管理',
+      //   component: () => import('@/views/conm/AttributeManagement/index'),
+      // },
+      // {
+      //   path: 'a_d_slot_management/:id',
+      //   name: '广告位管理',
+      //   component: () => import('@/views/conm/ADSlotManagement/index'),
+      // },
+      // {
+      //   path: 'a_d_management/:id',
+      //   name: '广告管理',
+      //   component: () => import('@/views/conm/ADManagement/index'),
+      // },
       {
         path: 'document_management/:id',
         name: '文档管理',

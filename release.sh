6d98f1bdc43063e9a964e7c8151873ee3e55f7f3
lanseria <zhangchao564265135@hotmail.com>
2019-09-06 17:13:55 +0800

    chore(bash): update

--- release.sh
+++ release.sh
@@ -5,6 +5,7 @@ git reset --hard origin/develop \
 && git merge origin/feature/govdata/dev \
 && git merge origin/feature/pg-big-data/dev \
 && git merge origin/fams/inverment \
-&& git merge origin/core/fix
+&& git merge origin/core/fix \
+&& git merge origin/feature/cfms/dev
 
 echo "Success!"

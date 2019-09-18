35523c33d7c8529373db20599655b63cffbae289
zhnagqi <850629074@qq.com>
2019-09-18 10:57:06 +0800

    fix: 产品
    产品

--- src/views/app/moduleDetails/Introduction.vue
+++ src/views/app/moduleDetails/Introduction.vue
@@ -14,8 +14,8 @@
         </div>
         <span class="updateTime">更新时间：{{form.updateTime}}</span>
         <el-button class="join" @click.stop="handleModuleClick">加入定制</el-button>
-        <el-button class="use">立即使用</el-button>
-        <el-button class="text">模块考试</el-button>
+        <el-button class="use" @click.stop="handleUseClick">立即使用</el-button>
+        <el-button class="text" @click.stop="handleTestClick">模块考试</el-button>
       </div>
     </el-card>
   </div>
@@ -39,6 +39,13 @@ export default {
       putModuleById(this.id).then(() => {
         this.$router.push('/app/resource/product_ku')
       })
+    },
+    handleUseClick () {
+      window.location.href = this.form.website
+    },
+    handleTestClick () {
+      const newUrl = this.form.exam_address ? this.form.exam_address : ''
+      this.$router.push(`${newUrl}`)
     },
   },
 }

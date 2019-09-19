e1962d924546b5f539000f96f59bf14c0bac450d
lanseria <zhangchao564265135@hotmail.com>
2019-09-19 19:24:20 +0800

    chore(update): bash

--- master.sh
+++ master.sh
@@ -2,6 +2,7 @@
 echo "Start Build New Master Branch!"
 
 git reset --hard origin/develop \
-&& git merge origin/feature/govdata/dev
+&& git merge origin/feature/govdata/dev \
+&& git merge origin/feature/people/dev
 
 echo "Success!"
--- release.sh
+++ release.sh
@@ -4,6 +4,7 @@ echo "Start Build New Release Branch!"
 git reset --hard origin/develop \
 && git merge origin/feature/govdata/dev \
 && git merge origin/feature/pg-big-data/dev \
-&& git merge origin/feature/cfms/dev
+&& git merge origin/feature/cfms/dev \
+&& git merge origin/feature/people/dev
 
 echo "Success!"

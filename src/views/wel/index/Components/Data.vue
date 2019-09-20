bcdd15d7a38f5cc1ec92494104ac06ca75f06291
zhangchao <zhangchao@126.com>
2019-09-20 17:07:46 +0800

    feat(工作台): 初始框架完成

---
+++ src/views/wel/index/Components/Data.vue
@@ -0,0 +1,18 @@
+<template>
+  <wrapper title="我的指数">
+    <div class="grid-btn-group">
+    </div>
+  </wrapper>
+</template>
+
+<script>
+import Wrapper from './Wrapper'
+export default {
+  components: {
+    Wrapper,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/wel/index/Components/Project.vue
@@ -0,0 +1,18 @@
+<template>
+  <wrapper title="我的项目">
+    <div class="grid-btn-group">
+    </div>
+  </wrapper>
+</template>
+
+<script>
+import Wrapper from './Wrapper'
+export default {
+  components: {
+    Wrapper,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>

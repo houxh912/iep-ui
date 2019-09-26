65145210d64d8f0253924eee7dbc28726f8a02db
zhangchao <zhangchao@126.com>
2019-09-26 09:16:44 +0800

    chore(update): bash

--- ican.sh
+++ ican.sh
@@ -3,8 +3,9 @@ echo "Start Build New Release Branch!"
 
 git reset --hard origin/i-can \
 && git merge origin/develop \
+&& git merge origin/feature/cfms/dev \
 && git push \
-&& git checkout deploy-i-can \
+&& git checkout deploy-release-ican \
 && git reset --hard origin/i-can \
 && git push
 

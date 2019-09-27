f80c927cf6fa13a109eba8663085831069aa9012
gold <291817075@qq.com>
2019-09-27 09:07:04 +0800

    fix(投资详情): 显示问题修复

--- src/views/app/wealth/wealthDetails/index.vue
+++ src/views/app/wealth/wealthDetails/index.vue
@@ -499,10 +499,9 @@ export default {
   overflow-x: hidden;
 }
 .wealth-details >>> .iep-ellipsis {
-  width: 33%;
   text-align: center;
 }
-.wealth-details >>> .el-scrollbar__bar.is-vertical > div{
+.wealth-details >>> .el-scrollbar__bar.is-vertical > div {
   height: 100%;
 }
 </style>

7d71a1c44b02050221265faa1ea2ec4fa993d0e1
zhangchao <zhangchao@126.com>
2019-10-12 11:06:37 +0800

    chore(fix): coflict

--- src/views/goms/Welcome/options.js
+++ src/views/goms/Welcome/options.js
@@ -1,9 +1,10 @@
 const initForm = () => {
   return {
     name: '',
-    logo: '',
+    logo: '//cloud.govmade.com/upload/iep/20199112/e7bab73c-675d-425b-8cd7-76a61472266e_TIM%E6%88%AA%E5%9B%BE20191012102432.png',
     intro: '',
     orgType: '1',
+    isICan: 0,
   }
 }
 

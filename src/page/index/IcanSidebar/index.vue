89d852ad15bf4b88775b08409dab970b288805b4
zhangchao <zhangchao@126.com>
2019-10-15 12:01:36 +0800

    style(et工作台): 菜单更新

--- src/page/index/IcanSidebar/index.vue
+++ src/page/index/IcanSidebar/index.vue
@@ -1,3 +1,28 @@
 <template>
-  <div></div>
+  <div class="et-menu-wrapper">
+    <div class="et-text">
+      ET
+    </div>
+  </div>
 </template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss" scoped>
+.et-menu-wrapper {
+  margin: 20px;
+  width: 200px;
+  padding: 5px 10px;
+  border-radius: 5px;
+  margin-bottom: 0;
+  border: 1px solid #eee;
+  .et-text {
+    text-align: center;
+    color: $--color-primary;
+    font-size: 30px;
+    font-weight: 600;
+  }
+}
+</style>

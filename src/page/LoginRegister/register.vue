067786f0077e4b0d540699cfe761b31c473bdd28
zhangchao <zhangchao@126.com>
2019-10-22 10:57:11 +0800

    chore(修复): 注册问题

--- src/page/LoginRegister/register.vue
+++ src/page/LoginRegister/register.vue
@@ -2,9 +2,17 @@
   <user-operation-layout>
     <div class="login-wrapper">
       <div class="title">
-        <h1>手机号注册</h1>
+        <div>
+          <el-button v-if="active===2" icon="el-icon-back" type="text" @click="handleBack">返回</el-button>
+        </div>
+        <div v-if="active===1" class="h1">手机号注册</div>
+        <div v-if="active===2" class="h1">确认密码</div>
+        <div>
+          <div style="width:55px;" v-if="active===2"></div>
+        </div>
       </div>
-      <register-mobile @onredirect="_goToRedirect"></register-mobile>
+      <register-mobile v-show="active===1" ref="RegisterMobile" @next="handleNext"></register-mobile>
+      <register-password v-show="active===2" ref="RegisterPassword" @onredirect="_goToRedirect"></register-password>
       <third></third>
     </div>
   </user-operation-layout>
@@ -12,22 +20,34 @@
 <script>
 import UserOperationLayout from './wrapper'
 import RegisterMobile from './Form/RegisterMobile'
+import RegisterPassword from './Form/RegisterPassword'
 import Third from './Form/Third'
 export default {
   components: {
     UserOperationLayout,
     RegisterMobile,
+    RegisterPassword,
     Third,
   },
   name: 'UserRegister',
   data () {
     return {
+      active: 1,
     }
   },
   methods: {
-    // handleHave () {
-    //   this.$openPage('/login?redirect=/wel/account-settings/binding')
-    // },
+    handleBack () {
+      this.active = 1
+      this.$refs['RegisterMobile'].form = {
+        mobile: '',
+        code: '',
+      }
+    },
+    handleNext (form) {
+      this.active = 2
+      this.$refs['RegisterPassword'].form.mobile = form.mobile
+      this.$refs['RegisterPassword'].form.code = form.code
+    },
     _goToRedirect () {
       if (this.$route.query.redirect) {
         this.$openPage(this.$route.query.redirect)
@@ -46,9 +66,10 @@ export default {
   .title {
     margin-bottom: 10px;
     display: flex;
-    justify-content: space-evenly;
+    justify-content: space-between;
+    align-items: center;
     width: 100%;
-    h1 {
+    .h1 {
       color: #888;
       font-size: 18px;
       cursor: pointer;

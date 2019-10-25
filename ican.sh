e7ceca29079ba55dd6d40ce7afda5d523415e6b2
zhangchao <zhangchao@126.com>
2019-10-25 09:29:13 +0800

    chore(update): bash

--- ican.sh
+++ ican.sh
@@ -5,6 +5,7 @@ git reset --hard origin/i-can \
 && git merge origin/develop \
 && git merge origin/feature/mcms/dev \
 && git merge origin/ics \
+&& git merge origin/feature/tms/dev \
 && git push \
 && git checkout deploy-release-ican \
 && git reset --hard origin/i-can \

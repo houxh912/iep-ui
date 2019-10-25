ceb90b0ea68124b39eef9a9b1201583de21acbb5
zhangchao <zhangchao@126.com>
2019-10-25 11:47:17 +0800

    feat(修复): 组织邀请

---
+++ src/views/goms/Components/MessageConfirm.vue
@@ -0,0 +1,23 @@
+<template>
+  <iep-dialog :dialog-show="dialogShow" title="邀请信息" width="500px">
+    <iep-input-area v-model="message"></iep-input-area>
+    <template slot="footer">
+      <iep-button type="primary" @click="handleOk()">批量邀请</iep-button>
+    </template>
+  </iep-dialog>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      dialogShow: false,
+      message: '',
+    }
+  },
+  methods: {
+    handleOk () {
+      this.$emit('on-message', this.message)
+    },
+  },
+}
+</script>
